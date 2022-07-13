6094f2b8e0c7c14bf9ea583f4dee08ec154ef58a
dhendryc <d.hendrych@web.de>
2022-07-13 15:55:02 +0200

    Added new files to the include list.

--- src/BranchWolfe.jl
+++ src/BranchWolfe.jl
@@ -18,11 +18,13 @@ const MOD = MathOptSetDistances
 include("time_tracking_lmo.jl")
 include("bounds.jl")
 include("node.jl")
+include("custom_bonobo.jl")
 include("callbacks.jl")
 include("problem.jl")
 include("infeasible_pairwise.jl")
 include("heuristics.jl")
 include("strong_branching.jl")
+include("utilities.jl")
 
 using Statistics
 using Random
