a436f77c484c4bf3e69a7bcd1d6c1d46064c6c3a
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-07-19 10:55:21 +0200

    added getters

--- src/interface.jl
+++ src/interface.jl
@@ -1,12 +1,31 @@
 
+import MathOptInterface
+const MOI = MathOptInterface
+
 function branch_wolfe(f, grad!, lmo; traverse_strategy = Bonobo.BFS(), kwargs...)
+
+    v_indices = MOI.get(lmo.o, MOI.ListOfVariableIndices())
+    if v_indices != MOI.VariableIndex.(1:length(v_indices))
+        error("Variables are expected to be contiguous and ordered from 1 to N")
+    end
     time_lmo = BranchWolfe.TimeTrackingLMO(lmo)
     
-    # TODO extract global bounds from constraints of the base lmo.o
-    global_bounds = BranchWolfe.IntegerBounds()
-
-    # TODO extract integer constraints from the lmo.o
     integer_variables = BitSet()
+    for cidx in MOI.get(lmo.o, MOI.ListOfConstraintIndices{MOI.VariableIndex, MOI.Integer}())
+        push!(integer_variables, cidx.value)
+    end
+    for cidx in MOI.get(lmo.o, MOI.ListOfConstraintIndices{MOI.VariableIndex, MOI.ZeroOne}())
+        push!(integer_variables, cidx.value)
+    end
+
+    global_bounds = BranchWolfe.IntegerBounds()
+    for idx in integer_variables
+        for ST in (MOI.LessThan{Float64}, MOI.GreaterThan{Float64})
+            cidx = MOI.ConstraintIndex{MOI.VariableIndex, ST}(idx)
+            s = MOI.get(lmo.o, MOI.ConstraintSet(), cidx)
+            push!(global_bounds, (idx, s))
+        end
+    end
 
     direction = Vector{Float64}(undef,a)
     Random.rand!(direction)
