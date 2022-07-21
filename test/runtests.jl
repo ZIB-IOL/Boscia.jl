98ff666f058800ca82fbbb84107928c8916694b9
Mathieu Besançon <mathieu.besancon@gmail.com>
2022-07-21 12:10:39 +0200

    dual gap

--- test/runtests.jl
+++ test/runtests.jl
@@ -219,7 +219,7 @@ const diff = Random.rand(Bool,n)*0.6.+0.3
     tree = Bonobo.initialize(; 
         traverse_strategy = Bonobo.BFS(),
         Node = typeof(nodeEx),
-        root = (problem=m, current_node_id = current_node_id = Ref{Int}(0), options= Dict{Symbol, Any}(:FW_tol => 1e-7, :verbose => false)),
+        root = (problem=m, current_node_id = current_node_id = Ref{Int}(0), options= Dict{Symbol, Any}(:FW_tol => 1e-7, :verbose => false, :dual_gap => 1e-6)),
     )
     Bonobo.set_root!(tree, 
         (active_set = active_set, 
@@ -279,7 +279,7 @@ end
     tree = Bonobo.initialize(;
         traverse_strategy = Bonobo.BFS(),
         Node = typeof(nodeEx),
-        root = (problem=m, current_node_id = current_node_id = Ref{Int}(0), options= Dict{Symbol, Any}(:FW_tol => 1e-7, :verbose => false)),
+        root = (problem=m, current_node_id = current_node_id = Ref{Int}(0), options= Dict{Symbol, Any}(:FW_tol => 1e-7, :verbose => false, :dual_gap => 1e-6)),
         branch_strategy = branching_strategy, #() ->
     )
     Bonobo.set_root!(tree, 
