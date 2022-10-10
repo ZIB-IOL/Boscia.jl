440683cdafccf6d25d771ca8c4a76d52f1e3ba42
hannahtro <jaimelabaguette@gmail.com>
2022-10-10 14:33:35 +0200

    exclude time outs in figure

--- examples/plot_boscia_vs_scip.jl
+++ examples/plot_boscia_vs_scip.jl
@@ -5,14 +5,20 @@ using CSV
 iter = 1
 
 df = DataFrame(CSV.File("examples/csv/boscia_vs_scip_1.csv"))
-df_temp = df#[19:nrow(df), :]
+#[19:nrow(df), :] 
 
-# display(df_temp)
+indices = [index for index in 1:nrow(df) if isodd(index)]
+df_boscia = copy(df)
+df_scip = copy(df)
+delete!(df_boscia, indices) 
+delete!(df_scip, indices) 
 
-indices = [index for index in 1:nrow(df_temp) if isodd(index)]
-delete!(df_temp, indices) 
+display(df_scip)
 
-display(df_temp)
+df_scip = filter(row -> !(row.termination_scip == "TIME_LIMIT"),  df_scip)
+#df_boscia = filter(row -> !(row.termination_boscia == "Time limit reached"),  df_boscia)
+
+display(df_scip)
 
 colors = ["b", "m", "c", "r", "g", "y", "k"]
 markers = ["o", "s", "^", "P", "X", "H", "D"]
@@ -28,12 +34,13 @@ PyPlot.matplotlib[:rc]("text.latex", preamble=raw"""
 ax = fig.add_subplot(111)
 #ax.scatter(df_temp[!,"dimension"], df_temp[!,"time_boscia"], label="Boscia", color=colors[1], marker=markers[1])
 #ax.scatter(df_temp[!,"dimension"], df_temp[!,"time_scip"], label="SCIP", color=colors[3], marker=markers[2])
-ax.plot(log.(sort(df_temp[!,"time_boscia"])), 1:nrow(df_temp), label="Boscia", color=colors[1], marker=markers[1])
-ax.plot(log.(sort(df_temp[!,"time_scip"])), 1:nrow(df_temp), label="SCIP", color=colors[3], marker=markers[2])
+ax.plot(sort(df_boscia[!,"time_boscia"]), 1:nrow(df_boscia), label="Boscia", color=colors[1], marker=markers[1])
+ax.plot(sort(df_scip[!,"time_scip"]), 1:nrow(df_scip), label="SCIP", color=colors[3], marker=markers[2])
 
-yticks(1:nrow(df_temp),[])
+yticks(1:nrow(df_boscia),[])
 #xlabel("Dimension")
-xlabel("log time in s")
+xlabel("Time in s")
+ax.set_xscale("log")
 ax.grid()
 fig.legend(loc=7, fontsize=12)
 fig.tight_layout()
