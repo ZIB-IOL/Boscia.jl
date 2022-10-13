46735763066fab68cae3c0f3e8b49341abbc5f10
hannahtro <jaimelabaguette@gmail.com>
2022-10-13 15:37:31 +0200

    fix types when reading csv

--- examples/boscia_vs_scip.jl
+++ examples/boscia_vs_scip.jl
@@ -79,9 +79,13 @@ function boscia_vs_scip(seed=1, dimension=5, iter=3)
         x, _, result = Boscia.solve(f, grad!, lmo; verbose=false, time_limit=limit)
         # @show x, f(x)
         time_boscia=result[:total_time_in_sec]
-        df = DataFrame(seed=seed, dimension=n, time_boscia=time_boscia, solution_boscia=result[:primal_objective], termination_boscia=result[:status], time_scip=-Inf, solution_scip=Inf, termination_scip=intial_status, ncalls_scip=-Inf)
+        if result[:status] == "Optimal (tolerance reached)"
+            status = "OPTIMAL"
+        end
+        df = DataFrame(seed=seed, dimension=n, time_boscia=time_boscia, solution_boscia=result[:primal_objective], termination_boscia=status, time_scip=-Inf, solution_scip=Inf, termination_scip=intial_status, ncalls_scip=-Inf)
         file_name = "examples/csv/boscia_vs_scip_mixed.csv"
         CSV.write(file_name, df, append=true)
+        display(df)
     end
 
     # @show x
@@ -133,7 +137,7 @@ function boscia_vs_scip(seed=1, dimension=5, iter=3)
         solution_scip = f(MOI.get(o, MOI.VariablePrimal(), x))
         # @show solution_scip
         termination_scip = String(string(MOI.get(o, MOI.TerminationStatus())))
-        df_temp = DataFrame(CSV.File("examples/csv/boscia_vs_scip_mixed.csv"))
+        df_temp = DataFrame(CSV.File("examples/csv/boscia_vs_scip_mixed.csv", types=Dict(:seed=>Int64, :dimension=>Int64, :time_boscia=>Float64, :solution_boscia=>Float64, :termination_boscia=>String, :time_scip=>Float64, :solution_scip=>Float64, :termination_scip=>String, :ncalls_scip=>Float64)))
         df_temp[nrow(df_temp)-iter+i, :time_scip] = time_scip
         df_temp[nrow(df_temp)-iter+i, :solution_scip] = solution_scip
         df_temp[nrow(df_temp)-iter+i, :termination_scip] = termination_scip
