e1091127c172d18186f125186219489b64fde662
hannahtro <hannahtroppens@gmail.com>
2022-10-27 19:01:27 +0200

    completed run afw mixed

--- examples/boscia_vs_afw.jl
+++ examples/boscia_vs_afw.jl
@@ -24,8 +24,8 @@ function boscia_vs_afw(seed=1, dimension=5, iter=3)
     @assert isposdef(Mi)
 
     # integer set
-    #I = 1:(n÷2)
-    I = collect(1:n)
+    I = 1:(n÷2)
+    #I = collect(1:n)
     
     o = SCIP.Optimizer()
     MOI.set(o, MOI.Silent(), true)
--- examples/run_boscia_vs_afw.jl
+++ examples/run_boscia_vs_afw.jl
@@ -1,7 +1,7 @@
 include("boscia_vs_afw.jl")
 
-for dimension in [20:5:50;]
-    for seed in 1:7
+for dimension in [20:5:80;]
+    for seed in 1:4
         @show seed, dimension
         boscia_vs_afw(seed, dimension, 1)
     end
