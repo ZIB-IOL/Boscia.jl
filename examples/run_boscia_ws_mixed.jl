3eb5e476f3467a823486d4eaf945e7c606ce191a
hannahtro <hannahtroppens@gmail.com>
2022-11-07 10:28:11 +0100

    completed run, warm up options

--- examples/run_boscia_ws_mixed.jl
+++ examples/run_boscia_ws_mixed.jl
@@ -1,21 +1,21 @@
 include("boscia_vs_afw.jl")
 
 
-for dimension in [20:5:80;]
-    for seed in 1:4
-        @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
-    end
-end
+#for dimension in [70:5:80;]
+#    for seed in 1:4
+#        @show seed, dimension
+#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
+#    end
+#end
 
-for dimension in [20:5:80;]
-    for seed in 1:4
-        @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
-    end
-end
+#for dimension in [80:5:80;]
+#    for seed in 3:4
+#        @show seed, dimension
+#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
+#    end
+#end
 
-for dimension in [20:5:80;]
+for dimension in [65:5:80;]
     for seed in 1:4
         @show seed, dimension
         boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
