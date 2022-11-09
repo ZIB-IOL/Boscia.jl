a7ef1dc45b9734fdc34d2aee890a6273303299b2
hannahtro <jaimelabaguette@gmail.com>
2022-11-09 12:27:26 +0100

    prepare run for missing instances

--- examples/run_boscia_ws_mixed.jl
+++ examples/run_boscia_ws_mixed.jl
@@ -15,9 +15,19 @@ include("boscia_vs_afw.jl")
 #    end
 #end
 
-for dimension in [65:5:80;]
+for dimension in [80:5:80;]
     for seed in 1:4
         @show seed, dimension
         boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
     end
 end
+
+boscia_vs_afw(3, 60, 1, mode="mixed", bo_mode="as_ss")
+boscia_vs_afw(4, 60, 1, mode="mixed", bo_mode="as_ss")
+boscia_vs_afw(4, 75, 1, mode="mixed", bo_mode="as_ss")
+
+boscia_vs_afw(4, 75, 1, mode="mixed", bo_mode="as")
+boscia_vs_afw(2, 80, 1, mode="mixed", bo_mode="as")
+boscia_vs_afw(4, 80, 1, mode="mixed", bo_mode="as")
+
+boscia_vs_afw(4, 65, 1, mode="mixed", bo_mode="ss")
