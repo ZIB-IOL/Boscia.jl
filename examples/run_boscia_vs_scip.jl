a40fd838578681c956349628863d8f60b65fb1e4
hannahtro <jaimelabaguette@gmail.com>
2022-11-09 16:31:40 +0100

    setup for additional seeds and dim 55,60

--- examples/run_boscia_vs_scip.jl
+++ examples/run_boscia_vs_scip.jl
@@ -1,11 +1,15 @@
 include("boscia_vs_scip.jl")
 
-# for dimension in [30:5:30;]
-#     for seed in 6:7
-#         @show seed, dimension
-#         boscia_vs_scip(seed, dimension, 2)
-#     end
-# end
+for dimension in [20:5:50;]
+    for seed in 8:10
+        @show seed, dimension
+        boscia_vs_scip(seed, dimension, 2)
+    end
+end
 
-boscia_vs_scip(1, 70, 2)
-boscia_vs_scip(2, 55, 2)
+for dimension in [55:5:60;]
+    for seed in 1:10
+        @show seed, dimension
+        boscia_vs_scip(seed, dimension, 2)
+    end
+end
