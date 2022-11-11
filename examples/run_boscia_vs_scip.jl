5fda03f61d3cbfba558aa349294216e19480cd3b
hannahtro <jaimelabaguette@gmail.com>
2022-11-11 10:41:35 +0100

    setup run

--- examples/run_boscia_vs_scip.jl
+++ examples/run_boscia_vs_scip.jl
@@ -3,13 +3,20 @@ include("boscia_vs_scip.jl")
 for dimension in [20:5:50;]
     for seed in 8:10
         @show seed, dimension
-        boscia_vs_scip("integer", seed, dimension, 2)
+        boscia_vs_scip("integer", seed, dimension, 1)
     end
 end
 
 for dimension in [55:5:60;]
     for seed in 1:10
         @show seed, dimension
-        boscia_vs_scip("integer", seed, dimension, 2)
+        boscia_vs_scip("integer", seed, dimension, 1)
+    end
+end
+
+for dimension in [20:5:80;]
+    for seed in 5:10
+        @show seed, dimension
+        boscia_vs_scip("mixed", seed, dimension, 1)
     end
 end
