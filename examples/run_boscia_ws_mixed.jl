3f475e8fd834c5892dbf201a8e89d09c8ee3e916
hannahtro <jaimelabaguette@gmail.com>
2022-11-21 09:53:24 +0100

    setup run: no ws ss in higher dim

--- examples/run_boscia_ws_mixed.jl
+++ examples/run_boscia_ws_mixed.jl
@@ -1,12 +1,12 @@
 include("boscia_vs_afw.jl")
 
 
-#for dimension in [70:5:80;]
-#    for seed in 1:4
-#        @show seed, dimension
-#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
-#    end
-#end
+for dimension in [85:5:100;]
+   for seed in 1:10
+       @show seed, dimension
+       boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
+   end
+end
 
 #for dimension in [80:5:80;]
 #    for seed in 3:4
@@ -15,19 +15,10 @@ include("boscia_vs_afw.jl")
 #    end
 #end
 
-for dimension in [80:5:80;]
-    for seed in 1:4
-        @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
-    end
-end
+# for dimension in [80:5:80;]
+#     for seed in 1:4
+#         @show seed, dimension
+#         boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
+#     end
+# end
 
-boscia_vs_afw(3, 60, 1, mode="mixed", bo_mode="as_ss")
-boscia_vs_afw(4, 60, 1, mode="mixed", bo_mode="as_ss")
-boscia_vs_afw(4, 75, 1, mode="mixed", bo_mode="as_ss")
-
-boscia_vs_afw(4, 75, 1, mode="mixed", bo_mode="as")
-boscia_vs_afw(2, 80, 1, mode="mixed", bo_mode="as")
-boscia_vs_afw(4, 80, 1, mode="mixed", bo_mode="as")
-
-boscia_vs_afw(4, 65, 1, mode="mixed", bo_mode="ss")
