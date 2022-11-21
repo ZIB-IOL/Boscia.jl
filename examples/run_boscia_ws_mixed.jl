c869274be8135b595360dfa36a372168931367a2
hannahtro <jaimelabaguette@gmail.com>
2022-11-21 12:34:55 +0100

    prepare run mixed in higher dim

--- examples/run_boscia_ws_mixed.jl
+++ examples/run_boscia_ws_mixed.jl
@@ -2,23 +2,23 @@ include("boscia_vs_afw.jl")
 
 
 for dimension in [85:5:100;]
-   for seed in 1:10
-       @show seed, dimension
-       boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
-   end
+    for seed in 1:10
+        @show seed, dimension
+        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
+    end
 end
 
-#for dimension in [80:5:80;]
-#    for seed in 3:4
-#        @show seed, dimension
-#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
-#    end
-#end
+for dimension in [85:5:100;]
+    for seed in 1:10
+        @show seed, dimension
+        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
+    end
+end
 
-# for dimension in [80:5:80;]
-#     for seed in 1:4
-#         @show seed, dimension
-#         boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
-#     end
-# end
+for dimension in [85:5:100;]
+    for seed in 1:10
+        @show seed, dimension
+        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
+    end
+end
 
