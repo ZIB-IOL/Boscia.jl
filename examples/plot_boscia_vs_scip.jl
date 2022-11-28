250e6433f5e87e05e697576d15107be7bcc9288c
hannahtro <jaimelabaguette@gmail.com>
2022-11-28 15:27:23 +0100

    update integer plot

--- examples/plot_boscia_vs_scip.jl
+++ examples/plot_boscia_vs_scip.jl
@@ -2,15 +2,8 @@ using PyPlot
 using DataFrames
 using CSV
 
-function plot_boscia_vs_scip(mode; afw=false)
-    if mode == "integer"
-        # load file
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_vs_scip_int.csv")))
-    elseif mode == "mixed"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_vs_scip_mixed.csv")))
-    elseif mode == "mixed_lowdim"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_vs_scip_mixed_lowdim.csv")))
-    elseif mode == "mixed_50"
+function plot_boscia_vs_scip(mode; afw=false, ss=false, as=false, as_ss=false)
+    if mode == "mixed_50"
         df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_vs_scip_mixed_50.csv")))
     elseif mode == "integer_50"
         df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_vs_scip_integer_50.csv")))
@@ -20,15 +13,16 @@ function plot_boscia_vs_scip(mode; afw=false)
 
     # display(df)
 
-    indices = [index for index in 1:nrow(df) if isodd(index)]
+    # indices = [index for index in 1:nrow(df) if isodd(index)]
     df_boscia = copy(df)
     df_scip = copy(df)
-    delete!(df_boscia, indices)
-    delete!(df_scip, indices)
+    # delete!(df_boscia, indices)
+    # delete!(df_scip, indices)
 
     # display(df_scip)
     time_limit = 1800
     filter!(row -> !(row.termination_scip == "TIME_LIMIT"),  df_scip)
+    filter!(row -> !(row.time_scip >= time_limit),  df_scip)
     #filter!(row -> !(row.termination_scip == "OPTIMIZE_NOT_CALLED"),  df_boscia)
     #df_boscia = filter(row -> !(row.termination_boscia == "Time limit reached"),  df_boscia)
     filter!(row -> !(row.time_boscia >= time_limit),  df_boscia)
@@ -59,28 +53,72 @@ function plot_boscia_vs_scip(mode; afw=false)
     ax.plot(time_scip, [1:nrow(df_scip); nrow(df_scip)], label="SCIP+OA", color=colors[end], marker=markers[2])
     #yticks(0:2:nrow(df_boscia)+1, 0:2:nrow(df_boscia)+1)
     
-    if afw && mode == "mixed_50"
-        df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/afw_mixed_50.csv")))
-        filter!(row -> !(row.time_afw >= time_limit),  df_afw)
-        time_afw = sort(df_afw[!,"time_afw"])
-        push!(time_afw, 1.1 * time_limit)
-        ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="AFW", color=colors[4], marker=markers[3])
-        @show nrow(df_afw), nrow(df_boscia)
+    if afw 
+        if mode == "mixed_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/afw_mixed_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="AFW", color=colors[4], marker=markers[3])
+        elseif mode == "integer_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/afw_integer_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="AFW", color=colors[4], marker=markers[3])
+        end
     end
 
-    if afw && mode == "integer_50"
-        df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/afw_integer_50.csv")))
-        indices = [index for index in 1:nrow(df_afw) if isodd(index)]
-        delete!(df_afw, indices)
-        filter!(row -> !(row.time_afw >= time_limit),  df_afw)
-        time_afw = sort(df_afw[!,"time_afw"])
-        push!(time_afw, 1.1 * time_limit)
-        ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="AFW", color=colors[4], marker=markers[3])
-        @show nrow(df_afw), nrow(df_boscia)
+    if ss 
+        if mode == "mixed_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_ss_mixed_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no shadow set", color=colors[5], marker=markers[4])
+        elseif mode == "integer_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_ss_integer_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no shadow set", color=colors[5], marker=markers[4])
+        end
+    end
+
+    if as 
+        if mode == "mixed_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_as_mixed_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no active set", color=colors[6], marker=markers[5])
+        elseif mode == "integer_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_as_integer_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no active set", color=colors[6], marker=markers[5])
+        end
+    end
+
+    if as_ss 
+        if mode == "mixed_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_as_ss_mixed_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no warm start", color=colors[7], marker=markers[6])
+        elseif mode == "integer_50"
+            df_afw = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_warm_start_as_ss_integer_50.csv")))
+            filter!(row -> !(row.time_afw >= time_limit),  df_afw)
+            time_afw = sort(df_afw[!,"time_afw"])
+            push!(time_afw, 1.1 * time_limit)
+            ax.plot(time_afw, [1:nrow(df_afw); nrow(df_afw)], label="no warm start", color=colors[7], marker=markers[6])
+        end
     end
 
     ylabel("Solved instances")
-    locator_params(axis="y", nbins=4)
+    #locator_params(axis="y", nbins=4)
     xlabel("Time (s)")
     ax.set_xscale("log")
     ax.grid()
@@ -96,13 +134,8 @@ function plot_boscia_vs_scip(mode; afw=false)
         fancybox=true, shadow=false, ncol=3)
     end
     fig.tight_layout()
-    if mode == "integer"
-        file = ("examples/csv/boscia_vs_scip.pdf")
-    elseif mode == "mixed"
-        file = ("examples/csv/boscia_vs_scip_mixed.pdf")
-    elseif mode == "mixed_lowdim"
-        file = ("examples/csv/boscia_vs_scip_mixed_lowdim.pdf")
-    elseif mode == "mixed_50"
+
+    if mode == "mixed_50"
         file = ("examples/csv/boscia_vs_scip_mixed_50.pdf")
     elseif mode == "integer_50"
         file = ("examples/csv/boscia_vs_scip_integer_50.pdf")
