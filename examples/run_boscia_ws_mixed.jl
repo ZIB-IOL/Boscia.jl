46644b2c49df07ccacb6eaa51fe066614a20d672
hannahtro <hannahtroppens@gmail.com>
2022-11-28 10:23:24 +0100

    completed run, dim 100

--- examples/run_boscia_ws_mixed.jl
+++ examples/run_boscia_ws_mixed.jl
@@ -1,24 +1,24 @@
 include("boscia_vs_afw.jl")
 
 
-for dimension in [85:5:100;]
-    for seed in 1:10
-        @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
-    end
-end
+#for dimension in [85:5:100;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="ss")
+#    end
+#end
+
+#for dimension in [85:5:100;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
+#    end
+#end
 
 for dimension in [85:5:100;]
     for seed in 1:10
         @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as")
-    end
-end
-
-for dimension in [85:5:100;]
-    for seed in 1:10
-        @show seed, dimension
-        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="as_ss")
+        boscia_vs_afw(seed, dimension, 1, mode="mixed", bo_mode="afw")
     end
 end
 
