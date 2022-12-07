7dbd7128097ae6ab68c2f5db7025e0a0231f5a2d
hannahtro <jaimelabaguette@gmail.com>
2022-12-07 11:09:45 +0100

    exclude boscia

--- examples/run_poisson.jl
+++ examples/run_poisson.jl
@@ -1,21 +1,21 @@
 include("poisson_reg.jl")
 
-bo_mode="boscia"
-for dimension in [70:20:100;]
-    for seed in 1:10
-        for ns in [0.1,1,5,10]
-            @show seed, dimension
-            try 
-                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
-            catch e 
-                println(e)
-                open("poisson_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+# bo_mode="boscia"
+# for dimension in [70:20:100;]
+#     for seed in 1:10
+#         for ns in [0.1,1,5,10]
+#             @show seed, dimension
+#             try 
+#                 poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
+#             catch e 
+#                 println(e)
+#                 open("poisson_errors.txt","a") do io
+#                     println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
+#                 end
+#             end
+#         end
+#     end
+# end
 
 bo_mode="as"
 for dimension in [50:20:100;]
