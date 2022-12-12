0adc1ec9c584206d3e74b54668da7ddf530f7c5b
hannahtro <hannahtroppens@gmail.com>
2022-12-12 14:33:40 +0100

    prepare ns 5.0 run scip oa

--- examples/run_poisson.jl
+++ examples/run_poisson.jl
@@ -17,78 +17,78 @@ include("poisson_reg.jl")
 #     end
 # end
 
-bo_mode="as"
-for dimension in [50:20:100;]
-    for seed in 1:10
-        for ns in [0.1,1,5,10]
-            @show seed, dimension
-            try 
-                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
-            catch e 
-                println(e)
-                open("poisson_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+#bo_mode="as"
+#for dimension in [50:20:100;]
+#    for seed in 1:10
+#        for ns in [0.1,1,5,10]
+#            @show seed, dimension
+#            try 
+#                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
+#            catch e 
+#                println(e)
+#                open("poisson_errors.txt","a") do io
+#                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
+#                end
+#            end
+#        end
+#    end
+#end
 
-bo_mode="ss"
-for dimension in [50:20:100;]
-    for seed in 1:10
-        for ns in [0.1,1,5,10]
-            @show seed, dimension
-            try 
-                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
-            catch e 
-                println(e)
-                open("poisson_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+#bo_mode="ss"
+#for dimension in [50:20:100;]
+#    for seed in 1:10
+#        for ns in [0.1,1,5,10]
+#            @show seed, dimension
+#            try 
+#                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
+#            catch e 
+#                println(e)
+#                open("poisson_errors.txt","a") do io
+#                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
+#                end
+#            end
+#        end
+#    end
+#end
 
-bo_mode="as_ss"
-for dimension in [50:20:100;]
-    for seed in 1:10
-        for ns in [0.1,1,5,10]
-            @show seed, dimension
-            try 
-                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
-            catch e 
-                println(e)
-                open("poisson_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+#bo_mode="as_ss"
+#for dimension in [50:20:100;]
+#    for seed in 1:10
+#        for ns in [0.1,1,5,10]
+#            @show seed, dimension
+#            try 
+#                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
+#            catch e 
+#                println(e)
+#                open("poisson_errors.txt","a") do io
+#                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
+#                end
+#            end
+#        end
+#    end
+#end
 
-bo_mode="afw"
-for dimension in [50:20:100;]
-    for seed in 1:10
-        for ns in [0.1,1,5,10]
-            @show seed, dimension
-            try 
-                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
-            catch e 
-                println(e)
-                open("poisson_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+#bo_mode="afw"
+#for dimension in [50:20:100;]
+#    for seed in 1:10
+#        for ns in [0.1,1,5,10]
+#            @show seed, dimension
+#            try 
+#                poisson(seed, dimension, ns, 1; bo_mode=bo_mode)
+#            catch e 
+#                println(e)
+#                open("poisson_errors.txt","a") do io
+#                    println(io, seed, " ", dimension, " ", ns, " ", bo_mode, " : ", e)
+#                end
+#            end
+#        end
+#    end
+#end
 
 bo_mode="scip_oa"
 for dimension in [50:20:100;]
     for seed in 1:10
-        for ns in [0.1,1,10]
+        for ns in [5.0]# [0.1,1,10]
             @show seed, dimension
             try 
                 poisson_scip(seed, dimension, ns, 1)            
