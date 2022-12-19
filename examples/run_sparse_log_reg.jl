b713a4e4901db8db594ea78598aaac6cd54ce8cb
hannahtro <hannahtroppens@gmail.com>
2022-12-19 18:05:55 +0100

    sparse log reg run

--- examples/run_sparse_log_reg.jl
+++ examples/run_sparse_log_reg.jl
@@ -1,22 +1,22 @@
 include("sparse_log_reg.jl")
 
 bo_mode="boscia"
-for dimension in [1:1:30;]
-    for seed in 1:3
-        @show seed, dimension
-        for ns in [0.1,1,5.0,10]
-            k = dimension/5
-            try 
-                sparse_log_regression(seed, dimension, 1; bo_mode=bo_mode)
-            catch e
-                println(e)
-                open("sparse_log_reg_errors.txt","a") do io
-                    println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-                end
-            end
-        end
-    end
-end
+#for dimension in [1:1:30;]
+#    for seed in 1:3
+#        @show seed, dimension
+#        for ns in [0.1,1,5.0,10]
+#            k = dimension/5
+#            try 
+#                sparse_log_regression(seed, dimension, 1; bo_mode=bo_mode)
+#            catch e
+#                println(e)
+#                open("sparse_log_reg_errors.txt","a") do io
+#                    println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#                end
+#            end
+#        end
+#    end
+#end
 
 #bo_mode = "as"
 # for dimension in [1:5:30;]
@@ -95,7 +95,7 @@ end
 # end
 
 bo_mode = "scip_oa"
-for dimension in [1:1:30;]
+for dimension in [1:1:20;]
     for seed in 1:3
         @show seed, dimension
         for ns in [0.1,1,5.0,10]
