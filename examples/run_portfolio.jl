dd19c80b5cfb6c7c45e2bbcfa2559035a7179f5d
hannahtro <jaimelabaguette@gmail.com>
2022-12-20 15:21:49 +0100

    restructure portfolio code

---
+++ examples/run_portfolio.jl
@@ -0,0 +1,96 @@
+include("portfolio.jl")
+
+include("boscia_vs_afw.jl")
+
+mode = "mixed"
+
+bo_mode="boscia"
+for dimension in [85:5:100;]
+    for seed in 1:10
+        @show seed, dimension
+        try 
+            portfolio(seed, dimension; bo_mode=bo_mode, mode=mode)
+        catch e
+            println(e)
+            open("portfolio_" * mode * "_errors.txt","a") do io
+                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
+
+# bo_mode="ss"
+# for dimension in [85:5:100;]
+#     for seed in 1:10
+#         @show seed, dimension
+#         try 
+#             portfolio(seed, dimension; bo_mode=bo_mode, mode=mode)
+#         catch e
+#             println(e)
+#             open("portfolio_" * mode * "_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
+
+# bo_mode="as"
+# for dimension in [85:5:100;]
+#     for seed in 1:10
+#         @show seed, dimension
+#         try 
+#             portfolio(seed, dimension; bo_mode=bo_mode, mode=mode)
+#         catch e
+#             println(e)
+#             open("portfolio_" * mode * "_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
+
+# bo_mode="as_ss"
+# for dimension in [85:5:100;]
+#     for seed in 1:10
+#         @show seed, dimension
+#         try 
+#             portfolio(seed, dimension; bo_mode=bo_mode, mode=mode)
+#         catch e
+#             println(e)
+#             open("portfolio_" * mode * "_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
+
+# bo_mode="afw"
+# for dimension in [85:5:100;]
+#     for seed in 1:10
+#         @show seed, dimension
+#         try 
+#             portfolio(seed, dimension; bo_mode=bo_mode, mode=mode)
+#         catch e
+#             println(e)
+#             open("portfolio_" * mode * "_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
+
+bo_mode = "scip_oa"
+for dimension in [85:5:100;]
+    for seed in 1:10
+        @show seed, dimension
+        try 
+            portfolio_scip(seed, dimension; mode="mixed")
+        catch e
+            println(e)
+            open("portfolio_" * mode * "_errors.txt","a") do io
+                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
+
