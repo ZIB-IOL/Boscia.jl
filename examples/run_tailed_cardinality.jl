52649fb8393553fecc4ecd88e5ee644bb06b7ada
hannahtro <jaimelabaguette@gmail.com>
2022-12-21 13:08:33 +0100

    change status_string

--- examples/run_tailed_cardinality.jl
+++ examples/run_tailed_cardinality.jl
@@ -1,19 +1,19 @@
 include("tailed_cardinality.jl")
 
-# bo_mode="boscia"
-# for dimension in [50:10:200;]
-#     for seed in 1:3
-#         @show seed, dimension
-#         try 
-#             sparse_regression(seed, dimension; bo_mode=bo_mode)
-#         catch e
-#             println(e)
-#             open("tailed_cardinality_errors.txt","a") do io
-#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-#             end
-#         end
-#     end
-# end
+bo_mode="boscia"
+for dimension in [50:10:200;]
+    for seed in 1:3
+        @show seed, dimension
+        try 
+            sparse_regression(seed, dimension; bo_mode=bo_mode)
+        catch e
+            println(e)
+            open("tailed_cardinality_errors.txt","a") do io
+                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
 
 #bo_mode = "as"
 #for dimension in [1:1:10;]
@@ -75,17 +75,17 @@ include("tailed_cardinality.jl")
 #    end
 #end
 
-bo_mode = "scip_oa"
-for dimension in [50:10:190;]
-    for seed in 1:3
-        @show seed, dimension
-        try 
-            sparse_reg_scip(seed, dimension)
-        catch e
-            println(e)
-            open("tailed_cardinality_errors.txt","a") do io
-                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-            end
-        end
-    end
-end
+# bo_mode = "scip_oa"
+# for dimension in [50:10:190;]
+#     for seed in 1:3
+#         @show seed, dimension
+#         try 
+#             sparse_reg_scip(seed, dimension)
+#         catch e
+#             println(e)
+#             open("tailed_cardinality_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
