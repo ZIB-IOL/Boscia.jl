87e5c62d9c2e609f6634d96ff50b3edbe55e0148
hannahtro <jaimelabaguette@gmail.com>
2023-01-06 15:28:45 +0100

    prepare run tailed cardinality

--- examples/run_tailed_cardinality.jl
+++ examples/run_tailed_cardinality.jl
@@ -1,23 +1,23 @@
 include("tailed_cardinality.jl")
 
-# bo_mode="boscia"
-# for dimension in [50:10:200;]
-#     for seed in 1:3
-#         @show seed, dimension
-#         try 
-#             sparse_regression(seed, dimension; bo_mode=bo_mode)
-#         catch e
-#             println(e)
-#             open("tailed_cardinality_errors.txt","a") do io
-#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-#             end
-#         end
-#     end
-# end
+bo_mode="boscia"
+for dimension in [50:10:150;]
+    for seed in 1:10
+        @show seed, dimension
+        try 
+            sparse_regression(seed, dimension; bo_mode=bo_mode)
+        catch e
+            println(e)
+            open("tailed_cardinality_errors.txt","a") do io
+                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
 
 bo_mode = "as"
-for dimension in [50:10:200;]
-   for seed in 1:3
+for dimension in [50:10:150;]
+   for seed in 1:10
        @show seed, dimension
        try 
            sparse_regression(seed, dimension; bo_mode=bo_mode)
@@ -31,8 +31,8 @@ for dimension in [50:10:200;]
 end
 
 bo_mode = "ss"
-for dimension in [50:10:200;]
-   for seed in 1:3
+for dimension in [50:10:150;]
+   for seed in 1:10
        @show seed, dimension
        try 
            sparse_regression(seed, dimension; bo_mode=bo_mode)
@@ -46,8 +46,8 @@ for dimension in [50:10:200;]
 end
 
 bo_mode = "as_ss"
-for dimension in [50:10:200;]
-   for seed in 1:3
+for dimension in [50:10:150;]
+   for seed in 1:10
        @show seed, dimension
        try 
            sparse_regression(seed, dimension; bo_mode=bo_mode)
@@ -61,8 +61,8 @@ for dimension in [50:10:200;]
 end
 
 bo_mode = "afw"
-for dimension in [50:10:200;]
-   for seed in 1:3
+for dimension in [50:10:150;]
+   for seed in 1:10
        @show seed, dimension
        try 
            sparse_regression(seed, dimension; bo_mode=bo_mode)
@@ -76,8 +76,8 @@ for dimension in [50:10:200;]
 end
 
 bo_mode = "scip_oa"
-for dimension in [200:10:200;]
-    for seed in 1:3
+for dimension in [50:10:150;]
+    for seed in 1:10
         @show seed, dimension
         try 
             sparse_reg_scip(seed, dimension)
