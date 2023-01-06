44d56895b06d2178db254629df2eca14ecc4a315
hannahtro <jaimelabaguette@gmail.com>
2023-01-06 14:38:00 +0100

    prepare sparse log run

--- examples/run_sparse_log_reg.jl
+++ examples/run_sparse_log_reg.jl
@@ -1,26 +1,26 @@
 include("sparse_log_reg.jl")
 
-# bo_mode="boscia"
-# for dimension in [1:1:30;]
-#    for seed in 1:3
-#        @show seed, dimension
-#        for ns in [0.1,1,5.0,10]
-#            k = dimension/5
-#            try 
-#                sparse_log_regression(seed, dimension, ns, k; bo_mode=bo_mode)
-#            catch e
-#                println(e)
-#                open("sparse_log_reg_errors.txt","a") do io
-#                    println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-#                end
-#            end
-#        end
-#    end
-# end
+bo_mode="boscia"
+for dimension in [10:2:20;]
+   for seed in 1:10
+       @show seed, dimension
+       for ns in [0.1,1,5.0,10]
+           k = dimension/5
+           try 
+               sparse_log_regression(seed, dimension, ns, k; bo_mode=bo_mode)
+           catch e
+               println(e)
+               open("sparse_log_reg_errors.txt","a") do io
+                   println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+               end
+           end
+       end
+   end
+end
 
 bo_mode = "as"
-for dimension in [1:1:30;]
-    for seed in 1:3
+for dimension in [10:2:20;]
+    for seed in 1:10
         @show seed, dimension
         for ns in [0.1,1,5.0,10]
             k = dimension/5
@@ -37,8 +37,8 @@ for dimension in [1:1:30;]
 end
 
 bo_mode = "ss"
-for dimension in [1:5:30;]
-    for seed in 1:3
+for dimension in [10:2:20;]
+    for seed in 1:10
         @show seed, dimension
         for ns in [0.1,1,5.0,10]
             k = dimension/5
@@ -55,8 +55,8 @@ for dimension in [1:5:30;]
 end
 
 bo_mode = "as_ss"
-for dimension in [1:5:30;]
-    for seed in 1:3
+for dimension in [10:2:20;]
+    for seed in 1:10
         @show seed, dimension
         for ns in [0.1,1,5.0,10]
             k = dimension/5
@@ -73,8 +73,8 @@ for dimension in [1:5:30;]
 end
 
 bo_mode = "afw"
-for dimension in [1:5:30;]
-    for seed in 1:3
+for dimension in [10:2:20;]
+    for seed in 1:10
         @show seed, dimension
         for ns in [0.1,1,5.0,10]
             k = dimension/5
@@ -90,20 +90,19 @@ for dimension in [1:5:30;]
     end
 end
 
-# bo_mode = "scip_oa"
-# for dimension in [1:1:30;]
-#     for seed in 1:3
-#         @show seed, dimension
-#         for ns in [0.1,1,5.0,10]
-#             k = dimension/5
-#             try 
-#                 sparse_log_reg_scip(seed, dimension, ns, k)
-#             catch e
-#                 println(e)
-#                 open("sparse_log_reg_errors.txt","a") do io
-#                     println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-#                 end
-#             end
-#         end
-#     end
-# end
+bo_mode = "scip_oa"
+for dimension in [10:2:20;]
+    for seed in 1:10
+        @show seed, dimension
+        for ns in [0.1,1,5.0,10]
+            try 
+                sparse_log_reg_scip(seed, dimension, ns, k)
+            catch e
+                println(e)
+                open("sparse_log_reg_errors.txt","a") do io
+                    println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+                end
+            end
+        end
+    end
+end
