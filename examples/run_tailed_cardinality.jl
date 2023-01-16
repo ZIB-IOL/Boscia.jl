0afc7099af227d9f7a55297c2afc5fa413928e53
hannahtro <jaimelabaguette@gmail.com>
2023-01-16 15:17:16 +0100

    prepare run scip oa tailed cardinality

--- examples/run_tailed_cardinality.jl
+++ examples/run_tailed_cardinality.jl
@@ -1,79 +1,79 @@
 include("tailed_cardinality.jl")
 
-bo_mode="boscia"
-for dimension in [50:10:150;]
-    for seed in 1:10
-        @show seed, dimension
-        try 
-            sparse_regression(seed, dimension; bo_mode=bo_mode)
-        catch e
-            println(e)
-            open("tailed_cardinality_errors.txt","a") do io
-                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-            end
-        end
-    end
-end
+# bo_mode="boscia"
+# for dimension in [50:10:150;]
+#     for seed in 1:10
+#         @show seed, dimension
+#         try 
+#             sparse_regression(seed, dimension; bo_mode=bo_mode)
+#         catch e
+#             println(e)
+#             open("tailed_cardinality_errors.txt","a") do io
+#                 println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#             end
+#         end
+#     end
+# end
 
-bo_mode = "as"
-for dimension in [50:10:150;]
-   for seed in 1:10
-       @show seed, dimension
-       try 
-           sparse_regression(seed, dimension; bo_mode=bo_mode)
-       catch e
-           println(e)
-           open("tailed_cardinality_errors.txt","a") do io
-               println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-           end
-       end
-   end
-end
+# bo_mode = "as"
+# for dimension in [50:10:150;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        try 
+#            sparse_regression(seed, dimension; bo_mode=bo_mode)
+#        catch e
+#            println(e)
+#            open("tailed_cardinality_errors.txt","a") do io
+#                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#            end
+#        end
+#    end
+# end
 
-bo_mode = "ss"
-for dimension in [50:10:150;]
-   for seed in 1:10
-       @show seed, dimension
-       try 
-           sparse_regression(seed, dimension; bo_mode=bo_mode)
-       catch e
-           println(e)
-           open("tailed_cardinality_errors.txt","a") do io
-               println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-           end
-       end
-   end
-end
+# bo_mode = "ss"
+# for dimension in [50:10:150;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        try 
+#            sparse_regression(seed, dimension; bo_mode=bo_mode)
+#        catch e
+#            println(e)
+#            open("tailed_cardinality_errors.txt","a") do io
+#                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#            end
+#        end
+#    end
+# end
 
-bo_mode = "as_ss"
-for dimension in [50:10:150;]
-   for seed in 1:10
-       @show seed, dimension
-       try 
-           sparse_regression(seed, dimension; bo_mode=bo_mode)
-       catch e
-           println(e)
-           open("tailed_cardinality_errors.txt","a") do io
-               println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-           end
-       end
-   end
-end
+# bo_mode = "as_ss"
+# for dimension in [50:10:150;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        try 
+#            sparse_regression(seed, dimension; bo_mode=bo_mode)
+#        catch e
+#            println(e)
+#            open("tailed_cardinality_errors.txt","a") do io
+#                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#            end
+#        end
+#    end
+# end
 
-bo_mode = "afw"
-for dimension in [50:10:150;]
-   for seed in 1:10
-       @show seed, dimension
-       try 
-           sparse_regression(seed, dimension; bo_mode=bo_mode)
-       catch e
-           println(e)
-           open("tailed_cardinality_errors.txt","a") do io
-               println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-           end
-       end
-   end
-end
+# bo_mode = "afw"
+# for dimension in [50:10:150;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        try 
+#            sparse_regression(seed, dimension; bo_mode=bo_mode)
+#        catch e
+#            println(e)
+#            open("tailed_cardinality_errors.txt","a") do io
+#                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#            end
+#        end
+#    end
+# end
 
 bo_mode = "scip_oa"
 for dimension in [50:10:150;]
