7a4329ac6878d8ad73be66bf8becdf30d8b0fedf
dhendryc <d.hendrych@web.de>
2023-01-18 14:23:24 +0100

    Corrected time measurements.

--- examples/sparse_log_reg.jl
+++ examples/sparse_log_reg.jl
@@ -228,8 +228,8 @@ function sparse_log_reg_ipopt(seed = 1, n = 20, Ns= 1.0, k=5)
     list_time = []
     list_number_nodes = []
     callback = build_callback(list_lb, list_ub, list_time, list_number_nodes)
-    data = @timed BB.optimize!(bnb_model, callback=callback)
     time_ref = Dates.now()
+    data = @timed BB.optimize!(bnb_model, callback=callback)
     push!(list_lb, bnb_model.lb)
     push!(list_ub, bnb_model.incumbent)
     push!(list_time, float(Dates.value(Dates.now()-time_ref)))
