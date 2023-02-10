8753d5d9a8c26e6fcbcf8cca5283163e278bbdf0
hannahtro <jaimelabaguette@gmail.com>
2023-02-10 11:32:47 +0100

    add rel_dual_gap to csv

--- examples/sparse_log_reg.jl
+++ examples/sparse_log_reg.jl
@@ -58,7 +58,7 @@ function sparse_log_regression(seed=1, dimension=10, M=3, k=5.0, var_A=10; bo_mo
     if occursin("Optimal", result[:status])
         status = "OPTIMAL"
     end
-    df = DataFrame(seed=seed, dimension=dimension, var_A=var_A, p=p, k=k, M=M, time=total_time_in_sec, x=[x], solution=result[:primal_objective], termination=status, ncalls=result[:lmo_calls])
+    df = DataFrame(seed=seed, dimension=dimension, var_A=var_A, p=p, k=k, M=M, time=total_time_in_sec, x=[x], solution=result[:primal_objective], rel_dual_gap=result[:rel_dual_gap], termination=status, ncalls=result[:lmo_calls])
     if bo_mode ==  "afw"
         file_name = joinpath(@__DIR__, "csv/" * bo_mode * "_sparse_log_regression.csv")
     elseif bo_mode == "boscia"
