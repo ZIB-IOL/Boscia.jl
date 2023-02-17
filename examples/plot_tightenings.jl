77139755f4e42320c7ae41ed8903ec58c5bccd6d
hannahtro <jaimelabaguette@gmail.com>
2023-02-17 12:32:44 +0100

    add tightening plots

--- examples/plot_tightenings.jl
+++ examples/plot_tightenings.jl
@@ -2,20 +2,20 @@ using PyPlot
 using DataFrames
 using CSV
 
-function plot(example)
+# "sparse_reg", "30_1"
+# "sparse_log_reg", "18_1-1_1"
+function plot(example, setup)
     if example == "sparse_reg"
-        df_boscia = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_sparse_reg_30_1.csv")))
-        df_global_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/global_tightening_sparse_reg_30_1.csv")))
-        df_local_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/local_tightening_sparse_reg_30_1.csv")))
-        df_no_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_tightening_sparse_reg_30_1.csv")))
+        df_boscia = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_sparse_reg_" * setup * ".csv")))
+        df_global_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/global_tightening_sparse_reg_" * setup * ".csv")))
+        df_local_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/local_tightening_sparse_reg_" * setup * ".csv")))
+        df_no_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_tightening_sparse_reg_" * setup * ".csv")))
     elseif example == "sparse_log_reg"
-        df_boscia = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_sparse_log_regression_18_1-1_1.csv")))
-        df_global_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/global_tightening_sparse_log_regression_18_1-1_1.csv")))
-        df_local_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/local_tightening_sparse_log_regression_18_1-1_1.csv")))
-        df_no_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_tightening_sparse_log_regression_18_1-1_1.csv")))
+        df_boscia = DataFrame(CSV.File(joinpath(@__DIR__, "csv/boscia_sparse_log_regression_" * setup * ".csv")))
+        df_global_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/global_tightening_sparse_log_regression_" * setup * ".csv")))
+        df_local_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/local_tightening_sparse_log_regression_" * setup * ".csv")))
+        df_no_tightening = DataFrame(CSV.File(joinpath(@__DIR__, "csv/no_tightening_sparse_log_regression_" * setup * ".csv")))
     end
-
-
 
     colors = ["b", "m", "c", "r", "g", "y", "k", "peru"]
     markers = ["o", "s", "^", "P", "X", "H", "D"]
@@ -43,6 +43,6 @@ function plot(example)
     ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.3), fontsize=12,
         fancybox=true, shadow=false, ncol=2)
     fig.tight_layout()
-    file = joinpath(@__DIR__, "csv/" * example * "_tightenings.pdf")
+    file = joinpath(@__DIR__, "csv/" * example * "_" * setup * "_tightenings.pdf")
     savefig(file)
 end
