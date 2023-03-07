74887578a067dd001e7a264bf201449b3cadfdb4
hannahtro <jaimelabaguette@gmail.com>
2023-03-07 15:49:52 +0100

    run missing afw instances

--- examples/run_sparse_reg.jl
+++ examples/run_sparse_reg.jl
@@ -104,21 +104,198 @@ for dimension in [15:1:30;]
    end
 end =#
 
-bo_mode = "afw"
-for dimension in [15:1:30;]
-   for seed in 1:10
-       @show seed, dimension
-       try 
-           sparse_reg(seed, dimension, 1; bo_mode=bo_mode)
-       catch 
-           println(e)
-           open("sparse_reg_errors.txt","a") do io
-               println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
-           end
-       end
-   end
-end
+# bo_mode = "afw"
+# for dimension in [15:1:30;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        try 
+#            sparse_reg(seed, dimension, 1; bo_mode=bo_mode)
+#        catch 
+#            println(e)
+#            open("sparse_reg_errors.txt","a") do io
+#                println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+#            end
+#        end
+#    end
+# end
 
+bo_mode = "afw"
+try 
+    sparse_reg(4, 15, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(5, 15, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(7, 15, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(8, 15, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 16, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(8, 16, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(10, 17, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 18, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(5, 18, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(5, 19, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 20, 1; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 20, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 20, 5; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 20, 7; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 20, 8; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(1, 21, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(10, 22, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(10, 23, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(10, 24, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(10, 25, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(6, 26, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
+try 
+    sparse_reg(6, 28, 2; bo_mode=bo_mode)
+catch 
+    println(e)
+    open("sparse_reg_errors.txt","a") do io
+        println(io, seed, " ", dimension, " ", bo_mode, " : ", e)
+    end
+end
 #=bo_mode = "scip_oa"
 for dimension in [28:1:30;]
     for seed in 1:10
