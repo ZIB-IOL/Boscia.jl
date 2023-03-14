5ee8536516c37f24a434be1e113ec28b2242a581
hannahtro <jaimelabaguette@gmail.com>
2023-03-14 10:58:57 +0100

    prepare tightenin run mip lib

--- examples/run_mip_lib.jl
+++ examples/run_mip_lib.jl
@@ -16,6 +16,51 @@ for num_v in [7,8]#[4:1:8;]
     end
 end
 
+bo_mode = "no_tightening"
+for num_v in [7,8]#[4:1:8;]
+    for seed in [1,2] #1:3
+        @show seed, num_v
+        try 
+            mip_lib(seed, num_v, true; example=example, bo_mode=bo_mode)
+        catch e
+            println(e)
+            open("mip_lib_" * example * "_errors.txt","a") do io
+                println(io, seed, " ", num_v, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
+
+bo_mode = "local_tightening"
+for num_v in [7,8]#[4:1:8;]
+    for seed in [1,2] #1:3
+        @show seed, num_v
+        try 
+            mip_lib(seed, num_v, true; example=example, bo_mode=bo_mode)
+        catch e
+            println(e)
+            open("mip_lib_" * example * "_errors.txt","a") do io
+                println(io, seed, " ", num_v, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
+
+bo_mode = "global_tightening"
+for num_v in [7,8]#[4:1:8;]
+    for seed in [1,2] #1:3
+        @show seed, num_v
+        try 
+            mip_lib(seed, num_v, true; example=example, bo_mode=bo_mode)
+        catch e
+            println(e)
+            open("mip_lib_" * example * "_errors.txt","a") do io
+                println(io, seed, " ", num_v, " ", bo_mode, " : ", e)
+            end
+        end
+    end
+end
+
 bo_mode = "strong_convexity"
 # pg5_34 dim 5 seed 3 and 6:8 1:3
 for num_v in [7,8]#[4:1:8;]
