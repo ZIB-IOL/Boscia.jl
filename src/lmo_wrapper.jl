19dde3e1ddcfea226a4a7cf0edeb6ce1dcaed787
Deborah Hendrych <hendrych@zib.de>
2023-10-13 21:58:17 +0200

    BLMO wrapper and functions that have to be implemented.

--- src/lmo_wrapper.jl
+++ src/lmo_wrapper.jl
@@ -1,9 +1,9 @@
 """
     MILMO
 
-Supertype for the Mixed Integer Linear Minimization Oracles
+Supertype for the Bounded Integer Linear Minimization Oracles
 """
-abstract type MixedIntegerLinearMinimizationOracle end 
+abstract type BoundedLinearMinimizationOracle end 
 
 """
 Multiple dispatch of FrankWolfe.compute_extreme_point
@@ -14,11 +14,74 @@ function compute_extreme_point end
 """
 function optimize! end 
 
-function get_lower_bounds end
+"""
+Get list of lower bounds.
+"""
+function get_lower_bounds_list end
 
-function get_upper_bounds end 
+"""
+Get list of upper bounds.
+"""
+function get_upper_bounds_list end 
 
-function set_lower_bounds end
+"""
+Get lower bound of variable.
+"""
+function get_lower_bound end
 
-function set_upper_bounds end
+"""
+Get upper bound of variable.
+"""
+function get_upper_bound end
+
+"""
+Set new lower bound.
+"""
+function set_lower_bound! end
+
+"""
+Set new upper bound.
+"""
+function set_upper_bound! end
+
+"""
+Check if the bound constraint is on an integral variable.
+"""
+function is_constraint_on_int_var end
+
+"""
+Is the constraint part of this contraints set. Used to check membership to the global and node bounds.
+"""
+function is_constraint_in end
+
+"""
+Delete constraints that are not needed anymore.
+"""
+function delete_constraints! end
+
+"""
+Add new bound constraint.
+"""
+function add_constraint! end
+
+"""
+Get the variable of the bound constraint.
+"""
+function get_variable_index(blmo::BoundedLinearMinimizationOracle, c_idx)
+    return c_idx
+end
+
+"""
+Check that all node bounds were set correctly. Not necessary
+"""
+function check_bounds(blmo::BoundedLinearMinimizationOracle, node_bounds)
+    return true
+ end
+
+ """
+ Check that the problem is feasible, i.e. there are no contradicting constraints, and that it is bounded.
+ """
+function check_feasibility(blmo::BoundedLinearMinimizationOracle)
+    return 
+end
 
