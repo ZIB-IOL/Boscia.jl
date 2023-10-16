ccea3d998abf9d38416e296ddda3f6787fd7eb90
Hendrych <dhendryc@mob-009366.zib.de>
2023-10-16 10:43:12 +0200

    Bounded Linear Minimzation Oracle as subtype of Linear Minimization Oracle.

--- src/lmo_wrapper.jl
+++ src/lmo_wrapper.jl
@@ -1,24 +1,79 @@
 """
-    MILMO
+    BLMO
 
-Supertype for the Mixed Integer Linear Minimization Oracles
+Supertype for the Bounded Linear Minimization Oracles
 """
-abstract type MixedIntegerLinearMinimizationOracle end 
+abstract type BoundedLinearMinimizationOracle <: FrankWolfe.LinearMinimizationOracle end 
 
 """
-Multiple dispatch of FrankWolfe.compute_extreme_point
+Given a direction d solves the problem
+    min_x d^T x
+where x has to be an integer feasible point
 """
 function compute_extreme_point end 
 
 """
+CHECK IF NECESSARY
 """
 function optimize! end 
 
-function get_lower_bounds end
+"""
+Get the index of the integer variable the bound is working on.
+"""
+function get_int_var end
 
-function get_upper_bounds end 
+"""
+Get the list of lower bounds.
+"""
+function get_lower_bound_list end
+"""
+Get the list of upper bounds.
+"""
+function get_upper_bound_list end 
 
-function set_lower_bounds end
+"""
+Change the value of the bound c_idx.
+"""
+function set_bound! end
 
-function set_upper_bounds end
+"""
+Read bound value for c_idx.
+"""
+function get_bound end
+
+"""
+Check if the subject of the bound c_idx is an integer variable (recorded in int_vars).
+"""
+function is_constraint_on_int_var end
+
+"""
+To check if there is bound for the variable in the global or node bounds.
+"""
+function is_bound_in end
+
+"""
+Delete bounds.
+"""
+function delete_bounds! end
+
+"""
+Add bound constraint.
+"""
+function add_bound_constraint! end
+
+"""
+Check if the bounds were set correctly in build_LMO.
+Safety check only.
+"""
+function build_LMO_correct(blmo, node_bounds)
+    return true
+end
+
+"""
+Free model data from previous solve (if necessary).
+"""
+function free_model(blmo)
+    return true
+end
+
 
