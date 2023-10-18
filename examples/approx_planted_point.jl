297178b1cd341c4ccc9129b4345e833bd59f7f10
Hendrych <dhendryc@mob-009366.zib.de>
2023-10-18 18:46:40 +0200

    Approx planted point with LMO over cube.

--- examples/approx_planted_point.jl
+++ examples/approx_planted_point.jl
@@ -9,21 +9,12 @@ using Distributions
 import MathOptInterface
 const MOI = MathOptInterface
 
+include("cube_blmo.jl")
 
 n = 20
 diffi = Random.rand(Bool, n) * 0.6 .+ 0.3
 
-@testset "Approximate planted point" begin
-    o = SCIP.Optimizer()
-    MOI.set(o, MOI.Silent(), true)
-    MOI.empty!(o)
-    x = MOI.add_variables(o, n)
-    for xi in x
-        MOI.add_constraint(o, xi, MOI.GreaterThan(0.0))
-        MOI.add_constraint(o, xi, MOI.LessThan(1.0))
-        MOI.add_constraint(o, xi, MOI.ZeroOne()) # or MOI.Integer()
-    end
-    lmo = FrankWolfe.MathOptLMO(o)
+@testset "Approximate planted point - Integer" begin
 
     function f(x)
         return 0.5 * sum((x[i] - diffi[i])^2 for i in eachindex(x))
@@ -32,8 +23,41 @@ diffi = Random.rand(Bool, n) * 0.6 .+ 0.3
         @. storage = x - diffi
     end
 
-    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true)
+    @testset "Using SCIP" begin
+        o = SCIP.Optimizer()
+        MOI.set(o, MOI.Silent(), true)
+        MOI.empty!(o)
+        x = MOI.add_variables(o, n)
+        for xi in x
+            MOI.add_constraint(o, xi, MOI.GreaterThan(0.0))
+            MOI.add_constraint(o, xi, MOI.LessThan(1.0))
+            MOI.add_constraint(o, xi, MOI.ZeroOne()) # or MOI.Integer()
+        end
+        lmo = FrankWolfe.MathOptLMO(o)
 
-    @test x == round.(diffi)
-    @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-3)
+        x, _, result = Boscia.solve(f, grad!, lmo, verbose=true)
+
+        @test x == round.(diffi)
+        @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-3)
+
+        x_scip = x
+    end
+
+    @testset "Using Cube LMO" begin
+        int_vars = []
+        bin_vars = collect(1:n)
+        
+        bounds = Boscia.IntegerBounds()
+        for i in 1:n 
+            push!(bounds, (i, MOI.GreaterThan(0.0)))
+            push!(bounds, (i, MOI.LessThan(0.0)))
+        end
+        blmo = CubeBLMO(n, int_vars, bin_vars, bounds)
+
+        x, _, result = Boscia.solve(f, grad!, blmo, verbose =true)
+
+        @test x == round.(diffi)
+        @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-3)
+    end
 end
+
