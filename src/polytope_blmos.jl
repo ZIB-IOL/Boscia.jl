815ea54fca73f3efe039805b755fdef9a6a07a44
Deborah Hendrych <hendrych@zib.de>
2023-11-01 22:05:06 +0100

    Prob and Unit Simplex.

--- src/polytope_blmos.jl
+++ src/polytope_blmos.jl
@@ -9,6 +9,9 @@ struct CubeSimpleBLMO <: SimpleBoundableLMO
     int_vars::Vector{Int}
 end
 
+"""
+If the entry is positve, choose the lower bound. Else, choose the upper bound.
+"""
 function bounded_compute_extreme_point(sblmo::CubeSimpleBLMO, d, lb, ub, int_vars; kwargs...)
     v = zeros(length(d))
     for i in eachindex(d)
@@ -32,4 +35,87 @@ function is_linear_feasible(sblmo::CubeSimpleBLMO, v)
         end
     end
     return true
+end
+
+"""
+    ProbablitySimplexSimpleBLMO(N)
+
+Scaled Probability Simplex: ∑ x = N.
+"""
+struct ProbablitySimplexSimpleBLMOSimpleBLMO <: SimpleBoundableLMO
+    N::Float64
+end
+
+"""
+Assign the largest possible values to the entries corresponding to the smallest entries of d.
+"""
+function bounded_compute_extreme_point(sblmo::ProbablitySimplexSimpleBLMOSimpleBLMO, d, lb, ub, int_vars; kwargs...)
+    v = zeros(length(d))
+    indices = collect(1:length(d))
+    perm = sortperm(d)
+
+    for i in indices[perm]
+        if i in int_vars
+            idx = findfirst(x -> x == i, int_vars)
+            v[i] = min(ub[idx], sblmo.N - sum(v))
+        else
+            v[i] = N - sum(v)
+        end
+    end
+    return v
+end
+
+function is_linear_feasible(sblmo::ProbablitySimplexSimpleBLMOSimpleBLMO, v)
+    if sum(v .≥ 0) < length(v)
+        @debug "v has negative entries: $(v)"
+        return false
+    end
+    return isapprox(sum(v), sblmo.N, atol=1e-4, rtol=1e-2)
+end
+
+"""
+    UnitSimplexSimpleBLMO(N)
+
+Scaled Unit Simplex: ∑ x ≤ N.
+"""
+struct UnitSimplexSimpleBLMOSimpleBLMO <: SimpleBoundableLMO
+    N::Float64
+end
+
+"""
+For all positive entries of d, assign the corresponding lower bound.
+For non-positive entries, assign largest possible value in increasing order.
+"""
+function bounded_compute_extreme_point(sblmo::UnitSimplexSimpleBLMOSimpleBLMO, d, lb, ub, int_vars; kwargs...)
+    v = zeros(length(d))
+    # d[i] positive
+    idx_pos = findall(x-> x > 0, d)
+    for i in idx_pos
+        if i in int_vars
+            idx = findfirst(x -> x == i, int_vars)
+            v[i] = lb[idx]
+        else
+            v[i] = 0.0
+        end
+    end
+    # d[i] negative
+    idx_neg = setdiff(eachindex(d), idx_pos)
+    perm = sortperm(d[idx_neg])
+    for i in idx_neg[perm]
+        if i in int_vars
+            idx = findfirst(x -> x == i, int_vars)
+            v[i] = min(ub[idx], sblmo.N - sum(v))
+        else
+            v[i] = N - sum(v)
+        end
+    end
+    return v
+end
+
+function is_linear_feasible(sblmo::UnitSimplexSimpleBLMOSimpleBLMO, v)
+    if sum(v .≥ 0) < length(v)
+        @debug "v has negative entries: $(v)"
+        return false
+    end
+    return sum(v) ≤ sblmo.N + 1e-3
 end
