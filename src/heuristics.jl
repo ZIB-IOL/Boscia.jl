94d73d4c28dba14115107cdf90e8589e68c5837d
Hendrych <dhendryc@mob-009366.zib.de>
2023-11-15 15:09:45 +0100

    Add rounding as first standard heuristic.

--- src/heuristics.jl
+++ src/heuristics.jl
@@ -23,4 +23,35 @@ const MAX_PROB = 0.3
 Chooses heuristic by rolling a dice.
 """
 function pick_heuristic(heuristic_list)
+    return heuristic_list[1]
 end
+
+"""
+Simple rounding heuristic.
+"""
+function rounding_heuristics(tree::Bonobo.BnBTree, x)
+    x_rounded = copy(x)
+    for idx in tree.branching_indices
+        x_rounded[idx] = round(x[idx])
+    end
+
+    # check linear feasibility
+    if is_linear_feasible(tree.root.problem.tlmo, x_rounded) &&
+        is_integer_feasible(tree, x_rounded)
+        # evaluate f(rounded)
+        val = tree.root.problem.f(x_rounded)
+        if val < tree.incumbent
+            tree.root.updated_incumbent[] = true
+            node = tree.nodes[tree.root.current_node_id[]]
+            sol = FrankWolfeSolution(val, x_rounded, node, :rounded)
+            push!(tree.solutions, sol)
+            if tree.incumbent_solution === nothing ||
+                sol.objective < tree.incumbent_solution.objective
+                tree.incumbent_solution = sol
+            end
+            tree.incumbent = val
+            Bonobo.bound!(tree, node.id)
+        end
+    end
+end
+
