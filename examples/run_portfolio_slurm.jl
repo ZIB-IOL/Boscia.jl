50bdffe0f131b8f70938169d09bd56bb9372d617
Troppens <htroppen@htc-login002.zib.de>
2024-01-17 12:34:36 +0100

    small portfolio run pavito

--- examples/run_portfolio_slurm.jl
+++ examples/run_portfolio_slurm.jl
@@ -2,7 +2,7 @@ include("portfolio.jl")
 
 mode = "integer"
 
-for dimension in [20:5:120;]
+for dimension in [20] #[20:5:120;]
     for seed in 1:10
         @show seed, dimension
         run(`sbatch batch_portfolio.sh $seed $dimension $mode`)
