5745f82a0e47ce75f66eb455e174756230e636c3
Troppens <htroppen@htc-login002.zib.de>
2024-01-19 14:55:24 +0100

    pavito mixed portfolio run

--- examples/run_portfolio_slurm.jl
+++ examples/run_portfolio_slurm.jl
@@ -1,4 +1,13 @@
-mode = "integer"
+#mode = "integer"
+
+#for dimension in [20:5:120;]
+#    for seed in 1:10
+#        @show seed, dimension
+#        run(`sbatch batch_portfolio.sh $seed $dimension $mode`)
+#    end
+#end
+
+mode = "mixed"
 
 for dimension in [20:5:120;]
     for seed in 1:10
@@ -6,12 +15,3 @@ for dimension in [20:5:120;]
         run(`sbatch batch_portfolio.sh $seed $dimension $mode`)
     end
 end
-
-# mode = "mixed"
-
-# for dimension in [20:5:120;]
-#     for seed in 1:10
-#         @show seed, dimension
-#         run(`sbatch batch_portfolio.sh $seed $dimension $mode`)
-#     end
-# end
