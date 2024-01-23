6996722f056fd746096e7ecee9a39f497d5bd011
Troppens <htroppen@htc-login002.zib.de>
2024-01-23 15:29:50 +0100

    some pavito sparse reg instances

--- examples/run_sparse_reg_slurm.jl
+++ examples/run_sparse_reg_slurm.jl
@@ -1,5 +1,5 @@
-for dimension in 15:30
-    for seed in 1:10
+for dimension in 16:30
+    for seed in 1:1#10
         @show seed, dimension
         run(`sbatch batch_sparse_reg.sh $seed $dimension`)
     end
