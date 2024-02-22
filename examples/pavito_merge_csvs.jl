518a6c75d376a7d09cdbcd91e442fe15f57cf853
hannahtro <jaimelabaguette@gmail.com>
2024-02-22 11:23:01 +0100

    merge results into one csv

--- examples/pavito_merge_csvs.jl
+++ examples/pavito_merge_csvs.jl
@@ -17,7 +17,10 @@ function merge_csvs(example="sparse_reg", seeds=1:1, dimensions=15:30)
         end 
     else @error "not a valid example"
     end
-    
+    select!(df, Not(:termination))
+    df[!, "termination"] = ["ALMOST_LOCALLY_SOLVED"]
+    select!(df, Not(:time))
+    df[!, "time"] = [1800.0]
     deleteat!(df, 1)
 
     # add results
@@ -28,6 +31,7 @@ function merge_csvs(example="sparse_reg", seeds=1:1, dimensions=15:30)
                     df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/pavito_" * example * "_" * string(seed) * "_" * string(dimension) * ".csv")))
                     append!(df, df_temp)
                 catch e 
+                    println(e)
                 end 
             end 
         end
@@ -38,20 +42,21 @@ function merge_csvs(example="sparse_reg", seeds=1:1, dimensions=15:30)
                     df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/pavito_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
                     append!(df, df_temp)
                 catch e 
+                    println(e)
                 end
             end 
         end
     end
-    
+
     # save csv 
     file_name = joinpath(@__DIR__, "csv/pavito_" * example * ".csv")
     CSV.write(file_name, df, append=false)
 
 end
 
-merge_csvs()
+# merge_csvs()
 merge_csvs("portfolio_mixed", 1:1, 20:5:120)
 merge_csvs("portfolio_integer", 1:1, 20:5:120)
-merge_csvs("miplib_22433", 4:8, 1:1)
-# merge_csvs("miplib_neos5", 4:8, 1:1)
-merge_csvs("miplib_ran14x18-disj-8", 4:8, 1:1)
+# merge_csvs("miplib_22433", 4:8, 1:1)
+# # merge_csvs("miplib_neos5", 4:8, 1:1)
+# merge_csvs("miplib_ran14x18-disj-8", 4:8, 1:1)
