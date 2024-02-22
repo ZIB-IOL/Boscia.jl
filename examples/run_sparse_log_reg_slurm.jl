e85ee7fdf098f40930579ea27bf1d53fd577730f
Troppens <htroppen@htc-login001.zib.de>
2024-02-22 14:24:07 +0100

    results for subsest of pavito instances

--- examples/run_sparse_log_reg_slurm.jl
+++ examples/run_sparse_log_reg_slurm.jl
@@ -3,10 +3,10 @@ include("sparse_log_reg.jl")
 for dimension in [5]#:5:20;]
     for seed in 1:1#3
         @show seed, dimension
-        for ns in [0.1,1]
+        for M in [0.1,1]
             for var_A in [1,5]
                 k = Float64(dimension)
-                run(`sbatch batch_sparse_log_reg.sh $seed $dimension $ns $k $var_A`)
+                run(`sbatch batch_sparse_log_reg.sh $seed $dimension $M $k $var_A`)
             end
         end
     end
