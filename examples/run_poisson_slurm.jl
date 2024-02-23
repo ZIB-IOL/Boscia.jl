f41bd08853c369a1482a91a3a2590ea79bc44431
Troppens <htroppen@htc-login001.zib.de>
2024-02-23 10:24:25 +0100

    slurm outputs pavito run

--- examples/run_poisson_slurm.jl
+++ examples/run_poisson_slurm.jl
@@ -1,5 +1,5 @@
-for dimension in [50]#:20:100;]
-    for seed in 1:1#10
+for dimension in [50:20:100;]
+    for seed in 1:10
         for ns in [0.1,1,5,10]
             @show seed, dimension, ns
             run(`sbatch batch_poisson.sh $seed $dimension $ns`)
