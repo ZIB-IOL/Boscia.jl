cff3bf392e8b5389b3c2fe38f7583a9064ab7d0c
Deborah Hendrych <hendrych@zib.de>
2024-03-11 21:54:47 +0100

    Fix gradient.

--- test/time_limit.jl
+++ test/time_limit.jl
@@ -9,11 +9,19 @@ using Distributions
 import Bonobo
 using Printf
 using Dates
+using FiniteDifferences
 const MOI = MathOptInterface
 const MOIU = MOI.Utilities
 
 import MathOptSetDistances
 const MOD = MathOptSetDistances
+
+
+seed = rand(UInt64)
+seed = 0x4d25d9e3640a9324
+@show seed
+Random.seed!(seed)
+
 
 n = 15
 const ri = rand(n)
@@ -31,7 +39,7 @@ time_limit = 30.0
     MOI.set(o, MOI.Silent(), true)
     MOI.empty!(o)
     x = MOI.add_variables(o, n)
-    I = collect(1:n) #rand(1:n0, Int64(floor(n0/2)))
+    I = collect(1:n)
     for i in 1:n
         MOI.add_constraint(o, x[i], MOI.GreaterThan(0.0))
         if i in I
@@ -43,23 +51,28 @@ time_limit = 30.0
         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ai, x), 0.0),
         MOI.LessThan(bi),
     )
-    MOI.add_constraint(
-        o,
-        MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ones(n), x), 0.0),
-        MOI.GreaterThan(1.0),
-    )
+   # MOI.add_constraint(
+   #     o,
+   #     MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ones(n), x), 0.0),
+   #     MOI.GreaterThan(1.0),
+   # )
     lmo = FrankWolfe.MathOptLMO(o)
 
     function f(x)
-        return Ωi * (x' * Mi * x) - ri' * x
+        return 1 / 2 * Ωi * dot(x, Mi, x) - dot(ri, x)
     end
     function grad!(storage, x)
-        storage .= 2 * Mi * x - ri
+        mul!(storage, Mi, x, Ωi, 0)
+        storage .-= ri
         return storage
     end
+    
+    gradient = rand(n)
+    
+    @show check_gradients(grad!,f, gradient) 
 
     start_time = Dates.now()
-    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit)
+    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit, fw_verbose=true)
     time_taken = float(Dates.value(Dates.now() - start_time)) / 1000
     @test sum(ai' * x) <= bi + 1e-6
     @test f(x) <= f(result[:raw_solution]) + 1e-6
