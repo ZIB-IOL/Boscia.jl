08826e07f7052b9854e5d5e3092a660fb41a0dba
Deborah Hendrych <hendrych@zib.de>
2024-03-11 22:07:49 +0100

    Delete seed setting.

--- test/time_limit.jl
+++ test/time_limit.jl
@@ -9,18 +9,11 @@ using Distributions
 import Bonobo
 using Printf
 using Dates
-using FiniteDifferences
 const MOI = MathOptInterface
 const MOIU = MOI.Utilities
 
 import MathOptSetDistances
 const MOD = MathOptSetDistances
-
-
-seed = rand(UInt64)
-seed = 0x4d25d9e3640a9324
-@show seed
-Random.seed!(seed)
 
 
 n = 15
@@ -51,11 +44,6 @@ time_limit = 30.0
         MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ai, x), 0.0),
         MOI.LessThan(bi),
     )
-   # MOI.add_constraint(
-   #     o,
-   #     MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(ones(n), x), 0.0),
-   #     MOI.GreaterThan(1.0),
-   # )
     lmo = FrankWolfe.MathOptLMO(o)
 
     function f(x)
@@ -66,13 +54,9 @@ time_limit = 30.0
         storage .-= ri
         return storage
     end
-    
-    gradient = rand(n)
-    
-    @show check_gradients(grad!,f, gradient) 
 
     start_time = Dates.now()
-    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit, fw_verbose=true)
+    x, _, result = Boscia.solve(f, grad!, lmo, verbose=true, time_limit=time_limit)
     time_taken = float(Dates.value(Dates.now() - start_time)) / 1000
     @test sum(ai' * x) <= bi + 1e-6
     @test f(x) <= f(result[:raw_solution]) + 1e-6
