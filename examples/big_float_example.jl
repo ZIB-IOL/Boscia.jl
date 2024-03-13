0e4344d6a4a422442994dd65193bb8fee9cd848d
Hendrych <dhendryc@mob-009366.zib.de>
2024-03-13 12:05:10 +0100

    Example using Double Float.

--- examples/big_float_example.jl
+++ examples/big_float_example.jl
@@ -32,3 +32,31 @@ diffi = Random.rand(Bool, n) * 0.6 .+ 0.3
     end
     @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-3)
 end
+
+@testset "Using DoubleFloat" begin
+    function f(x)
+        x = DoubleFloat.(x)
+        return float(0.5 * sum((x[i] - diffi[i])^2 for i in eachindex(x)))
+    end
+    function grad!(storage, x)
+        x = DoubleFloat.(x)
+        @. storage = x - diffi
+        return float.(storage)
+    end
+
+    int_vars = collect(1:n)
+    lbs = zeros(n)
+    ubs = ones(n)
+
+    sblmo = Boscia.CubeSimpleBLMO(lbs, ubs, int_vars)
+    custom_heuristics= [Boscia.Heuristic(Boscia.rounding_lmo_01_heuristic, 0.7, :rounding_lmo_01_heuristic),
+    Boscia.Heuristic(Boscia.probability_rounding, 0.7, :probability_rounding)]
+
+    x, _, result =
+        Boscia.solve(f, grad!, sblmo, lbs[int_vars], ubs[int_vars], int_vars, n, verbose=true, time_limit=300, custom_heuristics = custom_heuristics)
+
+    if result[:total_time_in_sec] < 910
+        @test x == round.(diffi)
+    end
+    @test isapprox(f(x), f(result[:raw_solution]), atol=1e-6, rtol=1e-3)
+end
