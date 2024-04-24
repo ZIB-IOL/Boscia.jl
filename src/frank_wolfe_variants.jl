3c826adb8377d63cae8e94ebaa28915980c7838c
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-04-24 18:36:47 +0200

    Update frank_wolfe_variants.jl

--- src/frank_wolfe_variants.jl
+++ src/frank_wolfe_variants.jl
@@ -205,7 +205,7 @@ function solve_frank_wolfe(
         grad!,
         lmo,
         x0;
-        line_search::LineSearchMethod=Adaptive(),
+        line_search::FrankWolfe.LineSearchMethod=FrankWolfe.Adaptive(),
         epsilon=1e-7,
         max_iteration=10000,
         print_iter=1000,
