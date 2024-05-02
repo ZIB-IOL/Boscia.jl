a44a7c48583e8f65abc4f80f3ee93247b3797c76
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-05-02 18:23:26 +0200

    Update polytope_blmos.jl; missing argument

--- src/polytope_blmos.jl
+++ src/polytope_blmos.jl
@@ -61,7 +61,7 @@ function bounded_compute_inface_extreme_point(sblmo::CubeSimpleBLMO, x, directio
         
 end
 
-function bounded_dicg_maximum_step(sblmo::CubeSimpleBLMO, x, direction; kwargs...)
+function bounded_dicg_maximum_step(sblmo::CubeSimpleBLMO, x, direction, lb, ub; kwargs...)
     d = []
     for i in eachindex(direction)
         push!(d, direction[i])
