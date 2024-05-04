057d765c8fec163f914f613cc985a55a4737682b
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-05-04 20:14:05 +0200

    Update polytope_blmos.jl

--- src/polytope_blmos.jl
+++ src/polytope_blmos.jl
@@ -297,7 +297,7 @@ function bounded_compute_inface_extreme_point(sblmo::Union{ProbabilitySimplexSim
     for idx in a
         if idx in non_fixed_int_idx
             non_idx = findfirst(x -> x==idx, non_fixed_int_idx)
-            if non_idx = val_idx
+            if non_idx == val_idx
                 a[non_idx] = active_val
             else
                 a[non_idx] = 0.0
