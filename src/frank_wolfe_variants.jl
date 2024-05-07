abac966cfe94f20fcbb13fcc3086b5d934234a11
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-05-07 20:04:00 +0200

    Update frank_wolfe_variants.jl

--- src/frank_wolfe_variants.jl
+++ src/frank_wolfe_variants.jl
@@ -200,10 +200,6 @@ function solve_frank_wolfe(
     workspace=nothing,
 )
     x0 = FrankWolfe.compute_active_set_iterate!(active_set)
-    println("lmo:")
-    println(lmo.blmo.lower_bounds)
-    println(lmo.blmo.upper_bounds)
-    
     # Observe that the lazy flag is only observed if away_steps is set to true, so it can neglected. 
     x, _, primal, dual_gap, _ = FrankWolfe.decomposition_invariant_conditional_gradient(
         f,
