1db09b82d6669a8194edc56a55fbf1fc5bdcea1f
Hendrych <dhendryc@mob-009366.zib.de>
2024-05-14 15:45:39 +0200

    Options runs.

--- examples/run_sparse_reg_setup.jl
+++ examples/run_sparse_reg_setup.jl
@@ -2,13 +2,15 @@ include("sparse_reg.jl")
 
 seed = parse(Int64, ARGS[1])
 dimension = parse(Int64, ARGS[2])
-@show seed, dimension
+mode = ARGS[3]
+depth = parse(Int64, ARGS[4])
+@show seed, dimension, mode, depth
 
 try 
-    sparse_reg_pavito(seed, dimension)#, bo_mode="default")
+    sparse_reg_boscia(seed, dimension, bo_mode=mode, depth=depth)#, bo_mode="default")
 catch e 
     println(e)
-    file = "pavito_sparse_reg_" * str(seed) * "_" * str(dimension)    
+    file = "boscia_sparse_reg_" * mode * "_" str(seed) * "_" * str(dimension)    
     open(file * ".txt","a") do io
         println(io, e)
     end
--- examples/run_sparse_reg_slurm.jl
+++ examples/run_sparse_reg_slurm.jl
@@ -1,6 +1,19 @@
-for dimension in 15:30
-    for seed in 1:10
-        @show seed, dimension
-        run(`sbatch batch_sparse_reg.sh $seed $dimension`)
+
+modes = ["no_tightening", "global_tightening", "local_tightening", "afw", "no_ss", "no_as", "no_as_no_ss", "strong_branching", "hybrid_branching"]
+
+for mode in modes
+    if mode == "hybrid_branching"
+        depths = [1, 2, 5, 10, 20]
+    else
+        depths = [1]
+    end
+
+    for depth in depths
+        for dimension in 15:30
+            for seed in 1:10
+                @show seed, dimension
+                run(`sbatch batch_sparse_reg.sh $seed $dimension $mode $depth`)
+            end
+        end
     end
 end
