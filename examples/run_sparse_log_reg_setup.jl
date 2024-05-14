35a35661036eff324f8c6dd676e67cafc381ab3a
Hendrych <dhendryc@mob-009366.zib.de>
2024-05-14 15:42:08 +0200

    Settings run.

--- examples/run_sparse_log_reg_setup.jl
+++ examples/run_sparse_log_reg_setup.jl
@@ -5,13 +5,15 @@ dimension = parse(Int64, ARGS[2])
 M = parse(Float64, ARGS[3])
 k = parse(Float64, ARGS[4])
 var_A = parse(Int64, ARGS[5])
-@show seed, dimension, M, k, var_A
+mode = ARGS[6]
+depth = parse(Int64, ARGS[7])
+@show seed, dimension, M, k, var_A, mode, depth
 
 try 
-    sparse_log_reg_pavito(seed, dimension, M, k, var_A)#, bo_mode="default")
+    sparse_log_reg_boscia(seed, dimension, M, k, var_A, bo_mode=mode, depth=depth)#, bo_mode="default")
 catch e 
     println(e)
-    file = "pavito_sparse_log_reg_" * str(seed) * "_" * str(dimension)    
+    file = "boscia_sparse_log_reg_" * mode * "_" *str(seed) * "_" * str(dimension)    
     open(file * ".txt","a") do io
         println(io, e)
     end
--- examples/run_sparse_log_reg_slurm.jl
+++ examples/run_sparse_log_reg_slurm.jl
@@ -1,12 +1,25 @@
-include("sparse_log_reg.jl")    
+include("sparse_log_reg.jl")   
 
-for dimension in [5:5:20;]
-    for seed in 1:10
-        @show seed, dimension
-        for M in [0.1,1]
-            for var_A in [1,5]
-                k = Float64(dimension)
-                run(`sbatch batch_sparse_log_reg.sh $seed $dimension $M $k $var_A`)
+modes = ["no_tightening", "global_tightening", "local_tightening", "afw", "no_ss", "no_as", "no_as_no_ss", "strong_branching", "hybrid_branching"]
+
+for mode in modes
+    if mode == "hybrid_branching"
+        depths = [1, 2, 5, 10, 20]
+    else
+        depths = [1]
+    end
+
+    for depth in depths
+
+        for dimension in [5:5:20;]
+            for seed in 1:10
+            @show seed, dimension
+                for M in [0.1,1]
+                    for var_A in [1,5]
+                        k = Float64(dimension)
+                        run(`sbatch batch_sparse_log_reg.sh $seed $dimension $M $k $var_A $mode $depth`)
+                    end
+                end
             end
         end
     end
