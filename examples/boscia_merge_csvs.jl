45b6a7f9f1661fc1fa48234c16b3dee1dd6737c3
Deborah Hendrych <hendrych@zib.de>
2024-05-19 18:09:43 +0200

    Compiled Boscia results.

--- examples/boscia_merge_csvs.jl
+++ examples/boscia_merge_csvs.jl
@@ -4,27 +4,38 @@ using DataFrames
 function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimensions=15:30, Ns=[], k=[], var_A=[], M=[])
     # setup df
     name = mode in ["no_as","no_ss","no_as_no_ss"] ? "no_warm_start_" * mode : "boscia_" * mode
+    folder = if mode == "default"
+                ""
+    elseif contains(mode, "hybrid_branching")
+        "hybrid_branching"
+    else
+        mode
+    end
     if example == "sparse_reg" 
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_1_17.csv")))
+        if mode == "default"
+            df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * "1_15.csv")))
+        else
+            df =  DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * ".csv")))
+        end
     elseif occursin("mip_lib", example) 
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
     elseif example == "portfolio_mixed" 
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
     elseif example == "portfolio_integer"
         try 
-            df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
+            df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
         catch e 
-            df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(25) * "_" * string(1) * ".csv")))
+            df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(25) * "_" * string(1) * ".csv")))
         end
     elseif example == "sparse_log_reg"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(var_A[1]) * "_" * string(dimensions[1]*5) * "_" * string(Ns[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(var_A[1]) * "_" * string(dimensions[1]*5) * "_" * string(Ns[1]) * ".csv")))
     elseif example == "poisson_reg"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(float(dimensions[1]/2)) * "_" * string(Ns[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(float(dimensions[1]/2)) * "_" * string(Ns[1]) * ".csv")))
         @show df
     elseif example == "tailed_cardinality"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimensions[1]) * "_" * string(seeds[1]) * ".csv")))
     elseif example == "tailed_cardinality_sparse_log_reg"
-        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(var_A[1]) * "_" * string(M[1]) * ".csv")))
+        df = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seeds[1]) * "_" * string(dimensions[1]) * "_" * string(var_A[1]) * "_" * string(M[1]) * ".csv")))
     else 
         @error "not a valid example"
     end
@@ -39,7 +50,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
         for seed in seeds 
             for dimension in dimensions 
                 try 
-                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * ".csv")))
+                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * ".csv")))
                     append!(df, df_temp)
                 catch e 
                     println(e)
@@ -50,7 +61,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
         for seed in seeds 
             for dimension in dimensions 
                 try 
-                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
+                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
                     append!(df, df_temp)
                 catch e 
                     println(e)
@@ -61,13 +72,14 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
         for seed in seeds 
             for dimension in dimensions 
                 try 
-                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
+                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
                     append!(df, df_temp)
                 catch e 
                     println(e)
                 end
             end 
         end
+      #  println("\n")
     elseif example == "sparse_log_reg"
         for dimension in dimensions
             for seed in seeds
@@ -75,7 +87,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
                     for var in var_A
                         k = Float64(dimension)
                         try 
-                            df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * "_" * string(var) * "_" * string(dimension*5) * "_" * string(ns) * ".csv")))
+                            df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * "_" * string(var) * "_" * string(dimension*5) * "_" * string(ns) * ".csv")))
                             append!(df, df_temp)
                         catch e
                         print(e)
@@ -90,7 +102,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
             for seed in seeds
                 for ns in Ns
                     try 
-                        df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * "_" * string(float(p/2)) * "_" * string(ns) * ".csv")))
+                        df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * "_" * string(float(p/2)) * "_" * string(ns) * ".csv")))
                         append!(df, df_temp)
                     catch e 
                         println(e)
@@ -102,7 +114,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
         for dimension in dimensions
             for seed in seeds
                 try
-                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
+                    df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(dimension) * "_" * string(seed) * ".csv")))
                     append!(df, df_temp)
                 catch e
                     println(e)
@@ -115,7 +127,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
                 for m in M
                     for var in var_A
                         try
-                            df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * mode * "/" * name * "_" * example * "_" string(var) * "_" * string(m) * ".csv")))
+                            df_temp = DataFrame(CSV.File(joinpath(@__DIR__, "csv/Boscia/" * folder * "/" * name * "_" * example * "_" * string(seed) * "_" * string(dimension) * "_" * string(var) * "_" * string(m) * ".csv")))
                             append!(df, df_temp)
                         catch e
                             println(e)
@@ -135,7 +147,7 @@ function merge_csvs(;example="sparse_reg", mode="default", seeds=1:10, dimension
 
 end
 
-modes = ["defaut", "no_tightening", "global_tightening", "local_tightening", "afw", "no_ss", "no_as", "no_as_no_ss", "strong_branching", "hybrid_branching"]
+modes = ["default", "no_tightening", "global_tightening", "local_tightening", "afw", "no_ss", "no_as", "no_as_no_ss", "strong_branching", "hybrid_branching_1", "hybrid_branching_2", "hybrid_branching_5", "hybrid_branching_10", "hybrid_branching_20"]
 for mode in modes
 
 merge_csvs(
@@ -143,21 +155,21 @@ merge_csvs(
     mode = mode,
     seeds=1:10, 
     dimensions=15:30
-)
+) 
 
-merge_csvs(
+#=merge_csvs(
     example = "portfolio_mixed", 
     mode= mode, 
     seeds = 1:10, 
     dimensions = 20:5:120
-)
+)=# 
 
-merge_csvs(
+#=merge_csvs(
     example = "portfolio_integer", 
     mode = mode, 
     seeds = 1:10, 
     dimensions = 20:5:120
-)
+) =#
 
 merge_csvs(
     example = "mip_lib_22433", 
@@ -215,12 +227,11 @@ merge_csvs(
     example = "tailed_cardinality_sparse_log_reg",
     mode = mode,
     dimensions = [5:5:20;],
-    seeds = 1:10,
+    seeds = 1:3,
     var_A = [1,5],
     M = [0.1,1],
-)
+) 
 end
-
 merge_csvs(
     example = "mip_lib_22433", 
     mode = "strong_convexity",
