6c543691be42feffd1fa728218056270d6e22e31
Hendrych <dhendryc@htc-login001.zib.de>
2024-06-13 10:52:47 +0200

    Reduce number of seeds for decay experiments.

--- examples/run_sparse_reg_slurm.jl
+++ examples/run_sparse_reg_slurm.jl
@@ -14,11 +14,11 @@ for mode in modes
     if mode == "dual_gap_decay_factor"
         dual_gap_decay_factors = [0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 1.0]
         epsilons = [1e-2, 1e-3, 5e-3, 1e-4]
-        seeds = 1:5
+        seeds = 1:3
     else
         dual_gap_decay_factors = [0.0]
         epsilons = [0.0]
-        seeds = 1:5
+        seeds = 1:10
     end
 
     for depth in depths
