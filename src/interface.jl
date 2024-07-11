f448a3d6b13bd0f181dd10d44e0127a97aec7596
Leon <stanzel.leon@gmail.com>
2024-07-11 12:31:39 +0200

    made changes such that other branching_strategies continue to work.

--- src/interface.jl
+++ src/interface.jl
@@ -294,17 +294,22 @@ function solve(
     tree.root.options[:callback] = fw_callback
     tree.root.current_node_id[] = Bonobo.get_next_node(tree, tree.options.traverse_strategy).id
     #the following should create the arrays and vectors only on first function call and then use existing one
-    pseudos = sparse(
-        repeat(Boscia.get_integer_variables(branching_strategy.bounded_lmo), 2),
-        vcat(ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo))), 2*ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))), 
-        ones(2 * length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))
-        )
-    branch_tracker = sparse(
-        repeat(Boscia.get_integer_variables(branching_strategy.bounded_lmo), 2),
-        vcat(ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo))), 2*ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))), 
-        ones(Int64, 2 * length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))
-        )
-
+    if isa(branching_strategy, Boscia.PSEUDO_COST)
+        pseudos = sparse(
+            repeat(Boscia.get_integer_variables(branching_strategy.bounded_lmo), 2),
+            vcat(ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo))), 2*ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))), 
+            ones(2 * length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))
+            )
+        branch_tracker = sparse(
+            repeat(Boscia.get_integer_variables(branching_strategy.bounded_lmo), 2),
+            vcat(ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo))), 2*ones(length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))), 
+            ones(Int64, 2 * length(Boscia.get_integer_variables(branching_strategy.bounded_lmo)))
+            )
+    else
+        pseudos = sparse([1], [1], [0.0])
+            
+        branch_tracker = sparse([1], [1], Int[0])
+    end 
     Bonobo.optimize!(tree, pseudos, branch_tracker; callback=bnb_callback)
 
     x = postsolve(tree, result, time_ref, verbose, max_iteration_post)
