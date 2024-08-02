79d033217513123db77a0ad3005753edc5d3eb81
Leon <stanzel.leon@gmail.com>
2024-08-02 20:30:34 +0200

    added a function to write out the content of results into 3 CSV files

--- src/utilities.jl
+++ src/utilities.jl
@@ -230,6 +230,67 @@ function sparse_min_via_enum(f, n, k, values=fill(0:1, n))
 end
 
 
+function save_results(
+    result::Dict{Symbol, Any},
+    settings::String,
+    example_name::String,
+    seed::UInt64,
+    file_name::String,
+    over_write::Bool
+    )
+    
+    seed = string(seed)
+    l1 = []# save all keys with one entry only
+    l2 = []# save all vector results of length equal to that of result[:list_ub]
+    l3 = []# save all vector results of length equal to that of lmo_calls_per_layer
+    for key in keys(result)
+        if length(result[key]) == 1 || isa(result[key], String)
+            push!(l1, key)
+        elseif length(result[key]) == length(result[:list_ub])
+            push!(l2, key)
+        elseif length(result[key]) == length(result[:lmo_calls_per_layer])
+            push!(l3, key)
+        end
+    end
+    l11 = Dict(string(key) => result[key] for key in l1)
+    l22 = Dict(string(key) => result[key] for key in l2)
+    l33 = Dict(string(key) => result[key] for key in l3)
+    l11 = DataFrame(l11)
+    l11[:, :example_name] .= example_name
+    l11[:, :seed] .= seed
+    l11[:, :settings] .= settings
+
+    l22 = DataFrame(l22)
+    l22[:, :settings] .= settings
+    l22[:, :example_name] .= example_name
+    l22[:, :seed] .= seed
+
+    l33 = DataFrame(l33)
+    l33[:, :settings] .= settings
+    l33[:, :example_name] .= example_name
+    l33[:, :seed] .= seed
+
+    file_name1 = "./results/" * file_name * "_summary.csv"
+
+    
+    if over_write# will always over write file if true
+        append = false
+    else
+        if isfile(file_name1)# using this method the first line of the file will have column names
+            append = false
+        else
+            append = true
+        end
+    end
+    CSV.write(file_name1, l11, append= append)
+
+    file_name2 = "./results/" * file_name * ".csv"
+
+    CSV.write(file_name2, l22, append= append)
+    file_name3 = "./results/" * file_name * "_layers.csv"
+    CSV.write(file_name3, l33, append= append)
+end
+
 # utility function to print the values of the parameters
 _value_to_print(::Bonobo.BestFirstSearch) = "Move best bound"
 _value_to_print(::PartialStrongBranching) = "Partial strong branching"
