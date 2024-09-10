7aab85bc0c7c632852752cde1ae659c0c61e6716
Mathieu Besançon <mathieu.besancon@gmail.com>
2024-09-10 16:41:31 +0200

    Update src/callbacks.jl

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -2,7 +2,7 @@
 Frank-Wolfe Callback.
 
 Is called in every Frank-Wolfe iteration.
-Node evaluation can be dymamically stopped here.
+Node evaluation can be dynamically stopped here.
 Time limit is checked.
 If the vertex is providing a better incumbent, it is added as solution.
 """
