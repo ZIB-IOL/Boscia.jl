144899247d56a21f85f98252548a3fd0916d2369
Hendrych <dhendryc@mob-009366.zib.de>
2024-10-14 16:40:49 +0200

    Adapt sharpness constants.

--- examples/optimal_experiment_design.jl
+++ examples/optimal_experiment_design.jl
@@ -40,41 +40,29 @@ include("oed_utils.jl")
     https://github.com/ZIB-IOL/FrankWolfe.jl/blob/master/examples/optimal_experiment_design.jl
 """
 
-m = 40
+m = 50
 verbose = true
 
 ## A-Optimal Design Problem
-
 @testset "A-Optimal Design" begin
 
     Ex_mat, n, N, ub = build_data(m)
 
     # sharpness constants
-    σ = minimum(Ex_mat' * Ex_mat)
+    σ = minimum(eigvals(Ex_mat' * Ex_mat))
     λ_max = maximum(ub) * maximum([norm(Ex_mat[i,:])^2 for i=1:size(Ex_mat,1)])
     θ = 1/2
-    M = sqrt(λ_max^3/  n * σ^4)
+    M = sqrt(λ_max^3 / σ^4)
 
-
-    g, grad! = build_a_criterion(Ex_mat, build_safe=true)
+    g, grad! = build_a_criterion(Ex_mat, build_safe=false)
     blmo = build_blmo(m, N, ub)
     x0, active_set = build_start_point(Ex_mat, N, ub)
     z = greedy_incumbent(Ex_mat, N, ub)
     domain_oracle = build_domain_oracle(Ex_mat, n)
-    line_search = FrankWolfe.MonotonicGenericStepsize(FrankWolfe.Adaptive(), domain_oracle)
+    line_search = FrankWolfe.Secant(domain_oracle=domain_oracle)
     heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
 
     x, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, time_limit=10, verbose=false, domain_oracle=domain_oracle, custom_heuristics=[heu], sharpness_exponent=θ, sharpness_constant=M, line_search=line_search)
-
-    _, active_set = build_start_point(Ex_mat, N, ub)
-    z = greedy_incumbent(Ex_mat, N, ub)
-    x, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, domain_oracle=domain_oracle, custom_heuristics=[heu], sharpness_exponent=θ, sharpness_constant=M, line_search=line_search) #sharpness_exponent=θ, sharpness_constant=M,
-
-    gradient = similar(x)
-    grad!(gradient, x)
-    v = Boscia.compute_extreme_point(blmo, gradient)
-    dual_gap = FrankWolfe.dot(gradient, x) - FrankWolfe.dot(gradient, v)
-    @show dual_gap
 
     blmo = build_blmo(m, N, ub)
     _, active_set = build_start_point(Ex_mat, N, ub)
@@ -82,12 +70,17 @@ verbose = true
     domain_oracle = build_domain_oracle(Ex_mat, n)
     heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
 
-    x_s, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, line_search=FrankWolfe.Secant(domain_oracle=domain_oracle), domain_oracle=domain_oracle, sharpness_exponent=θ, sharpness_constant=M, custom_heuristics=[heu]) #sharpness_exponent=θ, sharpness_constant=M, 
+    x_s, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, line_search=FrankWolfe.Secant(domain_oracle=domain_oracle), domain_oracle=domain_oracle, sharpness_exponent=θ, sharpness_constant=M, custom_heuristics=[heu]) 
 
-    @show x
-    @show x_s
-    @show g(x), g(x_s)
-    @test isapprox(g(x), g(x_s), atol=1e-3, rtol=5e-2)
+    blmo = build_blmo(m, N, ub)
+    _, active_set = build_start_point(Ex_mat, N, ub)
+    z = greedy_incumbent(Ex_mat, N, ub)
+    domain_oracle = build_domain_oracle(Ex_mat, n)
+    heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
+
+    x_n, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, line_search=FrankWolfe.Secant(domain_oracle=domain_oracle), domain_oracle=domain_oracle, custom_heuristics=[heu]) 
+
+    @test isapprox(g(x_s), g(x_n), atol=1e-2, rtol=5e-2)
 end 
 
 ## D-Optimal Design Problem
@@ -96,21 +89,12 @@ end
     Ex_mat, n, N, ub = build_data(m)
 
     # sharpness constants
-    σ = minimum(Ex_mat' * Ex_mat)
+    σ = minimum(eigvals(Ex_mat' * Ex_mat))
     λ_max = maximum(ub) * maximum([norm(Ex_mat[i,:])^2 for i=1:size(Ex_mat,1)])
     θ = 1/2
-    M = sqrt(2 * λ_max^2/ n * σ^4 )
+    M = sqrt(2 * λ_max^2 / σ^4 )
 
-
-    g, grad! = build_d_criterion(Ex_mat, build_safe=true)
-    blmo = build_blmo(m, N, ub)
-    x0, active_set = build_start_point(Ex_mat, N, ub)
-    z = greedy_incumbent(Ex_mat, N, ub)
-    domain_oracle = build_domain_oracle(Ex_mat, n)
-    heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
-
-    x, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose,  domain_oracle=domain_oracle, sharpness_exponent=θ, sharpness_constant=M, custom_heuristics=[heu]) 
-
+    g, grad! = build_d_criterion(Ex_mat, build_safe=false)
     blmo = build_blmo(m, N, ub)
     x0, active_set = build_start_point(Ex_mat, N, ub)
     z = greedy_incumbent(Ex_mat, N, ub)
@@ -119,9 +103,14 @@ end
 
     x_s, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, line_search=FrankWolfe.Secant(domain_oracle=domain_oracle), domain_oracle=domain_oracle, sharpness_exponent=θ, sharpness_constant=M, custom_heuristics=[heu]) 
 
-    @show x 
-    @show x_s
-    @show g(x), g(x_s)
-    @test isapprox(g(x), g(x_s), atol=1e-4, rtol=1e-2)
+    blmo = build_blmo(m, N, ub)
+    x0, active_set = build_start_point(Ex_mat, N, ub)
+    z = greedy_incumbent(Ex_mat, N, ub)
+    domain_oracle = build_domain_oracle(Ex_mat, n)
+    heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
+
+    x_n, _, result = Boscia.solve(g, grad!, blmo, active_set=active_set, start_solution=z, verbose=verbose, line_search=FrankWolfe.Secant(domain_oracle=domain_oracle), domain_oracle=domain_oracle, sharpness_exponent=θ, sharpness_constant=M, custom_heuristics=[heu]) 
+
+    @test isapprox(g(x_s), g(x_n), atol=1e-3, rtol=5e-2)
 end
 
