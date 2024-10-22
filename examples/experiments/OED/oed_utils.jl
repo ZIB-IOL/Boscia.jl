8150719c4ffca21d935d47d2c9acc11da2d32363
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-10-22 23:48:49 +0200

    Add files via upload

---
+++ examples/experiments/OED/oed_utils.jl
@@ -0,0 +1,488 @@
+"""
+	build_data
+
+seed - for the Random functions.
+m    - number of experiments.
+fusion - boolean deiciding whether we build the fusion or standard problem.
+corr - boolean deciding whether we build the independent or correlated data.   
+"""
+function build_data(m)
+	# set up
+	n = Int(floor(m / 10))
+
+	B = rand(m, n)
+	B = B' * B
+	@assert isposdef(B)
+	D = MvNormal(randn(n), B)
+
+	A = rand(D, m)'
+	@assert rank(A) == n
+
+	N = floor(1.5 * n)
+	u = floor(N / 3)
+	ub = rand(1.0:u, m)
+
+	return A, n, N, ub
+end
+
+"""
+Build Probability Simplex BLMO for Boscia
+"""
+function build_blmo(m, N, ub)
+	simplex_lmo = Boscia.ProbabilitySimplexSimpleBLMO(N)
+	blmo = Boscia.ManagedBoundedLMO(simplex_lmo, fill(0.0, m), ub, collect(1:m), m)
+	return blmo
+end
+
+"""
+Build function and the gradient for the A-criterion. 
+There is the option to build a safe version of the objective and gradient. If x does not
+satisfy the domain oracle, infinity is returned. 
+If one builds the unsafe version, a FrankWolfe line search must be chosen that can take 
+a domain oracle as an input like Secant or MonotonicGenericStepSize. 
+"""
+function build_a_criterion(A; μ = 1e-4, build_safe = false)
+	m, n = size(A)
+	a = m
+	domain_oracle = build_domain_oracle(A, n)
+
+	function f_a(x)
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		U = cholesky(X)
+		X_inv = U \ I
+		return LinearAlgebra.tr(X_inv) / a
+	end
+
+	function grad_a!(storage, x)
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X * X)
+		F = cholesky(X)
+		for i in 1:length(x)
+			storage[i] = LinearAlgebra.tr(-(F \ A[i, :]) * transpose(A[i, :])) / a
+		end
+		return storage
+	end
+
+	function f_a_safe(x)
+		if !domain_oracle(x)
+			return Inf
+		end
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		X_inv = LinearAlgebra.inv(X)
+		return LinearAlgebra.tr(X_inv) / a
+	end
+
+	function grad_a_safe!(storage, x)
+		if !domain_oracle(x)
+			return fill(Inf, length(x))
+		end
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X * X)
+		F = cholesky(X)
+		for i in 1:length(x)
+			storage[i] = LinearAlgebra.tr(-(F \ A[i, :]) * transpose(A[i, :])) / a
+		end
+		return storage
+	end
+
+	if build_safe
+		return f_a_safe, grad_a_safe!
+	end
+
+	return f_a, grad_a!
+end
+
+"""
+Build function and gradient for the D-criterion.
+There is the option to build a safe version of the objective and gradient. If x does not
+satisfy the domain oracle, infinity is returned. 
+If one builds the unsafe version, a FrankWolfe line search must be chosen that can take 
+a domain oracle as an input like Secant or MonotonicGenericStepSize. 
+"""
+function build_d_criterion(A; μ = 0.0, build_safe = false)
+	m, n = size(A)
+	a = 1#m
+	domain_oracle = build_domain_oracle(A, n)
+
+	function f_d(x)
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		return float(-log(det(X)) / a)
+	end
+
+	function grad_d!(storage, x)
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		F = cholesky(X)
+		for i in 1:length(x)
+			storage[i] = 1 / a * LinearAlgebra.tr(-(F \ A[i, :]) * transpose(A[i, :]))
+		end
+		return storage
+	end
+
+	function f_d_safe(x)
+		if !domain_oracle(x)
+			return Inf
+		end
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		return float(-log(det(X)) / a)
+	end
+
+	function grad_d_safe!(storage, x)
+		if !domain_oracle(x)
+			return fill(Inf, length(x))
+		end
+		X = transpose(A) * diagm(x) * A + Matrix(μ * I, n, n)
+		X = Symmetric(X)
+		F = cholesky(X)
+		for i in 1:length(x)
+			storage[i] = 1 / a * LinearAlgebra.tr(-(F \ A[i, :]) * transpose(A[i, :]))
+		end
+		# https://stackoverflow.com/questions/46417005/exclude-elements-of-array-based-on-index-julia
+		return storage
+	end
+
+	if build_safe
+		return f_d_safe, grad_d_safe!
+	end
+
+	return f_d, grad_d!
+end
+
+"""
+Find n linearly independent rows of A to build the starting point.
+"""
+function linearly_independent_rows(A)
+	S = []
+	m, n = size(A)
+	for i in 1:m
+		S_i = vcat(S, i)
+		if rank(A[S_i, :]) == length(S_i)
+			S = S_i
+		end
+		if length(S) == n # we only n linearly independent points
+			return S
+		end
+	end
+	return S # then x= zeros(m) and x[S] = 1
+end
+
+function add_to_min(x, u)
+	perm = sortperm(x)
+	j = findfirst(x -> x != 0, x[perm])
+
+	for i in j:length(x)
+		if x[perm[i]] < u[perm[i]]
+			x[perm[i]] += 1
+			break
+		else
+			continue
+		end
+	end
+	return x
+end
+
+function remove_from_max(x)
+	perm = sortperm(x, rev = true)
+	j = findlast(x -> x != 0, x[perm])
+
+	for i in 1:j
+		if x[perm[i]] > 1
+			x[perm[i]] -= 1
+			break
+		else
+			continue
+		end
+	end
+	return x
+end
+
+"""
+Build start point used in FrankWolfe and Boscia for the A-Optimal and D-Optimal Design Problem.
+The functions are self concordant and so not every point in the feasible region
+is in the domain of f and grad!.
+"""
+function build_start_point(A, N, ub)
+	# Get n linearly independent rows of A
+	m, n = size(A)
+	S = linearly_independent_rows(A)
+	@assert length(S) == n
+
+	x = zeros(m)
+	E = []
+	V = Vector{Float64}[]
+
+	while !isempty(setdiff(S, findall(x -> !(iszero(x)), x)))
+		v = zeros(m)
+		while sum(v) < N
+			idx = isempty(setdiff(S, findall(x -> !(iszero(x)), v))) ? rand(setdiff(collect(1:m), S)) : rand(setdiff(S, findall(x -> !(iszero(x)), v)))
+			if !isapprox(v[idx], 0.0)
+				@debug "Index $(idx) already picked"
+				continue
+			end
+			v[idx] = min(ub[idx], N - sum(v))
+			push!(E, idx)
+		end
+		push!(V, v)
+		x = sum(V .* 1 / length(V))
+	end
+	unique!(V)
+	a = length(V)
+	x = sum(V .* 1 / a)
+	active_set = FrankWolfe.ActiveSet(fill(1 / a, a), V, x)
+
+	return x, active_set, S
+end
+
+function build_start_point_func(A, N)
+	S = linearly_independent_rows(A)
+	m, n = size(A)
+	@assert length(S) == n
+
+	# Get n linearly independent rows of A
+	function build_dicg_start_point(lmo::Boscia.TimeTrackingLMO)
+		lb = lmo.blmo.lower_bounds
+		ub = lmo.blmo.upper_bounds
+		int_vars = lmo.blmo.int_vars
+
+		if 0 in ub[S]
+			return nothing
+		end
+
+		x = zeros(m)
+		x[int_vars] = lb
+		E = []
+		V = Vector{Float64}[]
+		if isempty(setdiff(S, findall(x -> !(iszero(x)), x)))
+
+			if sum(x) == N
+				return nothing
+			end
+
+			while sum(x) < N
+				idx = rand(collect(1:m))
+				if !isapprox(x[idx], lb[idx])
+					@debug "Index $(idx) already picked"
+					continue
+				end
+				x[idx] += min(ub[idx] - lb[idx], N - sum(x))
+			end
+			return x
+
+		end
+
+
+		it = 1
+		while !isempty(setdiff(S, findall(x -> !(iszero(x)), x)))
+			v = zeros(m)
+			v[int_vars] = lb
+			if sum(v) == N
+				return nothing
+			end
+			while sum(v) < N
+				idx = isempty(setdiff(S, findall(x -> !(iszero(x)), v))) ? rand(setdiff(collect(1:m), S)) : rand(setdiff(S, findall(x -> !(iszero(x)), v)))
+				if !isapprox(v[idx], lb[idx])
+					@debug "Index $(idx) already picked"
+					continue
+				end
+				v[idx] += min(ub[idx] - lb[idx], N - sum(v))
+				push!(E, idx)
+			end
+			push!(V, v)
+			x = sum(V .* 1 / length(V))
+		end
+		unique!(V)
+		a = length(V)
+		x = sum(V .* 1 / a)
+
+		return x
+	end
+	return build_dicg_start_point
+end
+
+
+
+"""
+Create first incumbent for Boscia and custom BB in a greedy fashion.
+"""
+function greedy_incumbent(A, N, ub)
+	# Get n linearly independent rows of A
+	m, n = size(A)
+	S = linearly_independent_rows(A)
+	@assert length(S) == n
+
+	# set entries to their upper bound
+	x = zeros(m)
+	x[S] .= ub[S]
+
+	if isapprox(sum(x), N; atol = 1e-4, rtol = 1e-2)
+		return x
+	elseif sum(x) > N
+		while sum(x) > N
+			remove_from_max(x)
+		end
+	elseif sum(x) < N
+		S1 = S
+		while sum(x) < N
+			jdx = rand(setdiff(collect(1:m), S1))
+			x[jdx] = min(N - sum(x), ub[jdx])
+			push!(S1, jdx)
+			sort!(S1)
+		end
+	end
+	@assert isapprox(sum(x), N; atol = 1e-4, rtol = 1e-2)
+	@assert sum(ub - x .>= 0) == m
+	return x
+end
+
+"""
+Check if given point is in the domain of f, i.e. X = transpose(A) * diagm(x) * A 
+positive definite.
+
+(a) Check the rank of A restricted to the rows activated by x.
+(b) Check if the resulting information matrix A' * diagm(x) * A is psd.
+
+(b) is a bit faster for smaller dimensions (< 100). For larger (> 200) (a) is faster.
+"""
+function build_domain_oracle(A, n)
+	return function domain_oracle(x)
+		S = findall(x -> !iszero(x), x)
+		return length(S) >= n && rank(A[S, :]) == n
+	end
+end
+
+function build_domain_oracle2(A)
+	return function domain_oracle2(x)
+		return isposdef(Symmetric(A' * diagm(x) * A))
+	end
+end
+
+function Boscia.is_decomposition_invariant_oracle_simple(sblmo::Boscia.ProbabilitySimplexSimpleBLMO)
+	return true
+end
+
+
+function Boscia.bounded_compute_inface_extreme_point(
+	sblmo::Boscia.ProbabilitySimplexSimpleBLMO,
+	d,
+	x,
+	lb,
+	ub,
+	int_vars;
+	kwargs...,
+)
+	indices = collect(1:length(d))
+	a = zeros(length(d))
+
+	a[int_vars] = lb
+
+	fixed_vars = []
+
+	for i in indices
+		if i in int_vars
+			idx = findfirst(x -> x == i, int_vars)
+			if x[i] == lb[idx]
+				push!(fixed_vars, i)
+			elseif x[i] == ub[idx]
+				push!(fixed_vars, i)
+				a[i] = ub[idx]
+			end
+		else
+			if x[i] == 0.0
+				push!(fixed_vars, i)
+			end
+			if x[i] == sblmo.N
+				push!(fixed_vars, i)
+				a[i] = sblmo.N
+			end
+		end
+	end
+
+	if sum(a) == sblmo.N
+		return a
+	end
+
+	non_fixed_idx = setdiff(indices, fixed_vars)
+
+	d_updated = d[non_fixed_idx]
+	perm = sortperm(d_updated)
+	sorted = non_fixed_idx[perm]
+
+	for i in sorted
+		if i in int_vars
+			idx = findfirst(x -> x == i, int_vars)
+			a[i] += min(ub[idx] - lb[idx], sblmo.N - sum(a))
+		else
+			a[i] += sblmo.N - sum(a)
+		end
+		if sum(a) == sblmo.N
+			return a
+		end
+	end
+
+
+	return a
+end
+
+
+
+
+function Boscia.bounded_dicg_maximum_step(
+	sblmo::Boscia.ProbabilitySimplexSimpleBLMO,
+	direction,
+	x,
+	lb,
+	ub,
+	int_vars;
+	kwargs...,
+)
+	"""
+	gamma_max = one(eltype(direction))
+	@inbounds for idx in eachindex(x)
+		di = direction[idx]
+		if di < 0
+			gamma_max = min(gamma_max, ub[idx]-x[idx])
+		elseif di > 0
+			gamma_max = min(gamma_max, x[idx]-lb[idx])
+		end
+	end
+	return gamma_max
+
+	"""
+	# the direction should never violate the simplex constraint because it would correspond to a gamma_max > 1
+	gamma_max = one(eltype(direction))
+	@inbounds for idx in eachindex(x)
+		di = direction[idx]
+		if di > 0
+			gamma_max = min(gamma_max, (x[idx] - lb[idx]) / di)
+			# gamma_max = nextfloat(gamma_max, -1)
+
+		elseif di < 0
+			gamma_max = min(gamma_max, (ub[idx] - x[idx]) / -di)
+		end
+
+		if gamma_max == 0.0
+			return 0.0
+		end
+	end
+
+	# for idx in eachindex(int_vars)
+	# 	var = int_vars[idx]
+	# 	di = direction[var]
+	# 	if di > 0
+	# 		gamma_max = min(gamma_max, (x[var] - lb[idx]) / di)
+
+	# 	elseif di < 0
+	# 		gamma_max = min(gamma_max, (ub[idx] - x[var]) / -di)
+	# 	end
+
+	# 	if gamma_max == 0.0
+	# 		return 0.0
+	# 	end
+	# end
+
+	return gamma_max
+end
---
+++ examples/experiments/OED/optimal_experiment_design.jl
@@ -0,0 +1,274 @@
+using Boscia
+using Random
+using Distributions
+using LinearAlgebra
+using FrankWolfe
+using Statistics
+using DataFrames
+using CSV
+
+import Boscia.build_dicg_start_point
+
+
+
+# The function building the problem data and other structures is in a separate file.
+include("oed_utils.jl")
+
+"""
+	The Optimal Experiment Design Problem consists of choosing a subset of experiments
+	maximising the information gain. 
+	We generate the Experiment matrix A ∈ R^{mxn} randomly. The information matrix is a linear
+	map X(x) = A' * diag(x) * A. There exists different information measures Φ. 
+	We concentrate on the A-criterion and D-criterion, i.e. 
+
+		Trace(X^{-1})       (A-Criterion)
+	and
+		-logdet(X)          (D-Criterion).
+
+	Consequently, the optimization problems we want to solve are
+
+	min_x  Trace( (A' * diag(x) * A)^{-1} )
+	s.t.   ∑ x_i = N                            (A-Optimal Design Problem)
+		   0 ≤ x ≤ u
+
+		   min_x  -logdet(A' * diag(x) * A)
+	s.t.   ∑ x_i = N                            (D-Optimal Design Problem)
+		   0 ≤ x ≤ u
+
+	where N is our bugdet for the experiments, i.e. this is the amount of experiments
+	we can perform. We set N = 3/2 * n. The upperbounds u are randomly generated. 
+	
+	Also, check this paper: https://arxiv.org/abs/2312.11200 and the corresponding 
+	respository https://github.com/ZIB-IOL/OptimalDesignWithBoscia.
+
+	A continuous version of the problem can be found in the examples in FrankWolfe.jl:
+	https://github.com/ZIB-IOL/FrankWolfe.jl/blob/master/examples/optimal_experiment_design.jl
+"""
+
+function oed_boscia(
+	seed,
+	dim;
+	mode = "bpcg",
+	type = "A-Optimal",
+	line_search = nothing,
+	fw_epsilon = 1e-2,
+	use_DICG_warm_start = false,
+	verbose = true,
+	time_limit = 3600,
+	write = true,
+	variant = Boscia.BPCG(),
+	lazy = true,
+)
+	@show seed
+	Random.seed!(seed)
+
+	m = dim
+
+
+	Ex_mat, n, N, ub = build_data(m)
+
+	# sharpness constants
+	σ = minimum(Ex_mat' * Ex_mat)
+	λ_max = maximum(ub) * maximum([norm(Ex_mat[i, :])^2 for i ∈ 1:size(Ex_mat, 1)])
+	θ = 1 / 2
+
+	## A-Optimal Design Problem
+
+	if type == "A-Optimal"
+
+
+		# sharpness constants
+		M = sqrt(λ_max^3 / n * σ^4)
+
+
+		g, grad! = build_a_criterion(Ex_mat, build_safe = true)
+		blmo = build_blmo(m, N, ub)
+		x0, active_set = build_start_point(Ex_mat, N, ub)
+		Boscia.build_dicg_start_point = build_start_point_func(Ex_mat, N)
+		z = greedy_incumbent(Ex_mat, N, ub)
+		domain_oracle = build_domain_oracle(Ex_mat, n)
+		if mode == "dicg"
+			line_search = FrankWolfe.Adaptive()
+		else
+			line_search = FrankWolfe.MonotonicGenericStepsize(FrankWolfe.Adaptive(), domain_oracle)
+		end
+		heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
+
+		x, _, result =
+			Boscia.solve(
+				g,
+				grad!,
+				blmo,
+				active_set = active_set,
+				start_solution = z,
+				variant = variant,
+				verbose = false,
+				lazy = lazy,
+				domain_oracle = domain_oracle,
+				custom_heuristics = [heu],
+				sharpness_exponent = θ,
+				sharpness_constant = M,
+				line_search = line_search,
+				fw_epsilon = fw_epsilon,
+				time_limit = 10,
+				use_DICG_warm_start = use_DICG_warm_start,
+			) #sharpness_exponent=θ, sharpness_constant=M,
+
+		_, active_set = build_start_point(Ex_mat, N, ub)
+		z = greedy_incumbent(Ex_mat, N, ub)
+
+		x, _, result =
+			Boscia.solve(
+				g,
+				grad!,
+				blmo,
+				active_set = active_set,
+				start_solution = z,
+				variant = variant,
+				verbose = verbose,
+				lazy = lazy,
+				domain_oracle = domain_oracle,
+				custom_heuristics = [heu],
+				sharpness_exponent = θ,
+				sharpness_constant = M,
+				line_search = line_search,
+				fw_epsilon = fw_epsilon,
+				time_limit = time_limit,
+				use_DICG_warm_start = use_DICG_warm_start,
+			) #sharpness_exponent=θ, sharpness_constant=M,
+
+
+	elseif type == "D-Optimal"
+		## D-Optimal Design Problem
+
+		# sharpness constants
+		M = sqrt(2 * λ_max^2 / n * σ^4)
+
+		g, grad! = build_d_criterion(Ex_mat, build_safe = true)
+		blmo = build_blmo(m, N, ub)
+		x0, active_set = build_start_point(Ex_mat, N, ub)
+		build_dicg_start_point = build_start_point_func(Ex_mat, N)
+		z = greedy_incumbent(Ex_mat, N, ub)
+		domain_oracle = build_domain_oracle(Ex_mat, n)
+		if mode == "dicg"
+			line_search = FrankWolfe.Adaptive()
+		else
+			line_search = FrankWolfe.Secant(40, 1e-8, domain_oracle)
+		end
+		heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
+
+
+
+		x, _, result =
+			Boscia.solve(
+				g,
+				grad!,
+				blmo,
+				active_set = active_set,
+				start_solution = z,
+				variant = variant,
+				verbose = false,
+				lazy = lazy,
+				fw_epsilon = fw_epsilon,
+				domain_oracle = domain_oracle,
+				custom_heuristics = [heu],
+				sharpness_exponent = θ,
+				time_limit = 10,
+				sharpness_constant = M,
+				line_search = line_search,
+				use_DICG_warm_start = use_DICG_warm_start,
+			) #sharpness_exponent=θ, sharpness_constant=M,
+
+		blmo = build_blmo(m, N, ub)
+		x0, active_set = build_start_point(Ex_mat, N, ub)
+		z = greedy_incumbent(Ex_mat, N, ub)
+		domain_oracle = build_domain_oracle(Ex_mat, n)
+		heu = Boscia.Heuristic(Boscia.rounding_hyperplane_heuristic, 0.7, :hyperplane_aware_rounding)
+
+		x, _, result =
+			Boscia.solve(
+				g,
+				grad!,
+				blmo,
+				active_set = active_set,
+				start_solution = z,
+				variant = variant,
+				verbose = verbose,
+				lazy = lazy,
+				fw_epsilon = fw_epsilon,
+				domain_oracle = domain_oracle,
+				custom_heuristics = [heu],
+				sharpness_exponent = θ,
+				time_limit = time_limit,
+				sharpness_constant = M,
+				line_search = line_search,
+				use_DICG_warm_start = use_DICG_warm_start,
+			) #sharpness_exponent=θ, sharpness_constant=M,
+
+	end
+
+	total_time_in_sec = result[:total_time_in_sec]
+	status = result[:status]
+	if occursin("Optimal", result[:status])
+		status = "OPTIMAL"
+	elseif occursin("Time", result[:status])
+		status = "TIME_LIMIT"
+	end
+
+	if write
+		lb_list = result[:list_lb]
+		ub_list = result[:list_ub]
+		time_list = result[:list_time]
+		list_lmo_calls = result[:list_lmo_calls_acc]
+		list_active_set_size_cb = result[:list_active_set_size]
+		list_discarded_set_size_cb = result[:list_discarded_set_size]
+		list_local_tightening = result[:local_tightenings]
+		list_global_tightening = result[:global_tightenings]
+
+		dir_path = joinpath(@__DIR__, "./csv")
+
+		mkpath(dir_path)
+
+		# df_full = DataFrame(
+		# 	time = time_list / 1000,
+		# 	lowerBound = lb_list,
+		# 	upperBound = ub_list,
+		# 	termination = status,
+		# 	LMOcalls = list_lmo_calls,
+		# 	localTighteings = list_local_tightening,
+		# 	globalTightenings = list_global_tightening,
+		# 	list_active_set_size_cb = list_active_set_size_cb,
+		# 	list_discarded_set_size_cb = list_discarded_set_size_cb,
+		# )
+		# file_name_full = joinpath(
+		# 	dir_path,
+		# 	"full_run_boscia_" * mode * "_.csv",
+		# )
+		# CSV.write(file_name_full, df_full, append = false)
+
+
+
+		@show result[:primal_objective]
+		df = DataFrame(
+			time = total_time_in_sec,
+			solution = result[:primal_objective],
+			dual_gap = result[:dual_gap],
+			rel_dual_gap = result[:rel_dual_gap],
+			termination = status,
+			ncalls = result[:lmo_calls],
+		)
+
+
+
+		file_name = joinpath(
+			dir_path,
+			"boscia_" * mode * "_oed_" * string(dim) * "_" * string(seed) * ".csv",
+		)
+		CSV.write(file_name, df, append = false, writeheader = true)
+	end
+
+end
+
+oed_boscia(10, 80; mode = "bpcg", variant = Boscia.BPCG(), line_search = FrankWolfe.Adaptive(), lazy = false, write = false)
+
+# oed_boscia(2, 30;)
