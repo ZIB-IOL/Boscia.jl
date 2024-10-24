ac2a6e0bb00294b3067bfff667ddb8f6b672ab84
Wenjie Xiao <116566662+WenjieXiao-2022@users.noreply.github.com>
2024-10-24 16:40:40 +0200

    Update utilities.jl

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,37 +1,37 @@
 # Ultilities function 
 @inline function Base.setproperty!(c::AbstractFrankWolfeNode, s::Symbol, v)
-    if s in (
-        :id,
-        :lb,
-        :ub,
-    )
-        # To be bale to convert, we want the function defined in Base
-        # not in Core like in Bonobo.Ultilities.jl
-        Base.setproperty!(c.std, s, v) 
-    else
-        Core.setproperty!(c, s, v)
-    end
+	if s in (
+		:id,
+		:lb,
+		:ub,
+	)
+		# To be bale to convert, we want the function defined in Base
+		# not in Core like in Bonobo.Ultilities.jl
+		Base.setproperty!(c.std, s, v)
+	else
+		Core.setproperty!(c, s, v)
+	end
 end
 
 """
 Compute relative gap consistently everywhere
 """
 function relative_gap(primal, dual)
-    gap = if signbit(primal) != signbit(dual)
-        Inf
-    elseif primal == dual
-        0.0
-    else
-        (primal - dual) / min(abs(primal), abs(dual))
-    end
-    return gap
+	gap = if signbit(primal) != signbit(dual)
+		Inf
+	elseif primal == dual
+		0.0
+	else
+		(primal - dual) / min(abs(primal), abs(dual))
+	end
+	return gap
 end
 
 """
 Check feasibility and boundedness
 """
 function check_feasibility(tlmo::TimeTrackingLMO)
-    return check_feasibility(tlmo.blmo)
+	return check_feasibility(tlmo.blmo)
 end
 
 
@@ -39,15 +39,15 @@ end
 Check if at a given index we have an integer constraint respectivily.
 """
 function has_integer_constraint(tree::Bonobo.BnBTree, idx::Int)
-    return has_integer_constraint(tree.root.problem.tlmo.blmo, idx)
+	return has_integer_constraint(tree.root.problem.tlmo.blmo, idx)
 end
 
 
 """
 Check wether a split is valid. 
 """
-function is_valid_split(tree::Bonobo.BnBTree, vidx::Int)
-    return is_valid_split(tree, tree.root.problem.tlmo.blmo, vidx)
+function is_valid_split(tree::Bonobo.BnBTree, var::Int)
+	return is_valid_split(tree, tree.root.problem.tlmo.blmo, var)
 end
 
 
@@ -56,15 +56,15 @@ Call this if the active set is empty after splitting.
 Remark: This should not happen when using a MIP solver for the nodes!
 """
 function restart_active_set(
-    node::FrankWolfeNode,
-    lmo::FrankWolfe.LinearMinimizationOracle,
-    nvars::Int,
+	node::FrankWolfeNode,
+	lmo::FrankWolfe.LinearMinimizationOracle,
+	nvars::Int,
 )
-    direction = Vector{Float64}(undef, nvars)
-    Random.rand!(direction)
-    v = compute_extreme_point(lmo, direction)
-    push!(node.active_set, (1.0, v))
-    return node.active_set
+	direction = Vector{Float64}(undef, nvars)
+	Random.rand!(direction)
+	v = compute_extreme_point(lmo, direction)
+	push!(node.active_set, (1.0, v))
+	return node.active_set
 end
 
 
@@ -72,115 +72,162 @@ end
 Split an active set between left and right children.
 """
 function split_vertices_set!(
-    active_set::FrankWolfe.ActiveSet{T,R},
-    tree,
-    var::Int,
-    local_bounds::IntegerBounds;
-    atol=1e-5,
-    rtol=1e-5,
-) where {T,R}
-    x = FrankWolfe.get_active_set_iterate(active_set)
-    right_as =
-        FrankWolfe.ActiveSet{Vector{Float64},Float64,Vector{Float64}}([], [], similar(active_set.x))
-    # indices to remove later from the left active set
-    left_del_indices = BitSet()
-    for (idx, tup) in enumerate(active_set)
-        (λ, a) = tup
-        if !is_bound_feasible(local_bounds, a)
-            @info "removed"
-            push!(left_del_indices, idx)
-            continue
-        end
-        # if variable set to 1 in the atom,
-        # place in right branch, delete from left
-        if a[var] >= ceil(x[var]) || isapprox(a[var], ceil(x[var]), atol=atol, rtol=rtol)
-            push!(right_as, tup)
-            push!(left_del_indices, idx)
-        elseif a[var] <= floor(x[var]) || isapprox(a[var], floor(x[var]), atol=atol, rtol=rtol)
-            # keep in left, don't add to right
-        else #floor(x[var]) < a[var] < ceil(x[var])
-            # if you are in middle, delete from the left and do not add to the right!
-            @warn "Attention! Vertex in the middle."
-            push!(left_del_indices, idx)
-        end
-    end
-    deleteat!(active_set, left_del_indices)
-    @assert !isempty(active_set)
-    @assert !isempty(right_as)
-    # renormalize active set and recompute new iterates
-    if !isempty(active_set)
-        FrankWolfe.active_set_renormalize!(active_set)
-        FrankWolfe.compute_active_set_iterate!(active_set)
-    end
-    if !isempty(right_as)
-        FrankWolfe.active_set_renormalize!(right_as)
-        FrankWolfe.compute_active_set_iterate!(right_as)
-    end
-    return (active_set, right_as)
+	active_set::FrankWolfe.ActiveSet{T, R},
+	tree,
+	var::Int,
+	local_bounds::IntegerBounds;
+	atol = 1e-5,
+	rtol = 1e-5,
+) where {T, R}
+	x = FrankWolfe.get_active_set_iterate(active_set)
+	right_as =
+		FrankWolfe.ActiveSet{Vector{Float64}, Float64, Vector{Float64}}([], [], similar(active_set.x))
+	# indices to remove later from the left active set
+	left_del_indices = BitSet()
+	for (idx, tup) in enumerate(active_set)
+		(λ, a) = tup
+		if !is_bound_feasible(local_bounds, a)
+			@info "removed"
+			push!(left_del_indices, idx)
+			continue
+		end
+		# if variable set to 1 in the atom,
+		# place in right branch, delete from left
+		if a[var] >= ceil(x[var]) || isapprox(a[var], ceil(x[var]), atol = atol, rtol = rtol)
+			push!(right_as, tup)
+			push!(left_del_indices, idx)
+		elseif a[var] <= floor(x[var]) || isapprox(a[var], floor(x[var]), atol = atol, rtol = rtol)
+			# keep in left, don't add to right
+		else #floor(x[var]) < a[var] < ceil(x[var])
+			# if you are in middle, delete from the left and do not add to the right!
+			@warn "Attention! Vertex in the middle."
+			push!(left_del_indices, idx)
+		end
+	end
+	deleteat!(active_set, left_del_indices)
+	@assert !isempty(active_set)
+	@assert !isempty(right_as)
+	# renormalize active set and recompute new iterates
+	if !isempty(active_set)
+		FrankWolfe.active_set_renormalize!(active_set)
+		FrankWolfe.compute_active_set_iterate!(active_set)
+	end
+	if !isempty(right_as)
+		FrankWolfe.active_set_renormalize!(right_as)
+		FrankWolfe.compute_active_set_iterate!(right_as)
+	end
+	return (active_set, right_as)
 end
 
-function split_pre_computed_set!(x, pre_computed_set::Vector, tree, vidx::Int;atol=1e-5,rtol=1e-5,kwargs...)
-    pre_computed_set_left = []
-    pre_computed_set_right = []
-    for atom in pre_computed_set
-        if atom[vidx] >= ceil(x[vidx]) || isapprox(atom[vidx], ceil(x[vidx]), atol=atol, rtol=rtol)
-            push!(pre_computed_set_right, atom)
-        elseif atom[vidx] <= floor(x[vidx]) || isapprox(atom[vidx], floor(x[vidx]), atol=atol, rtol=rtol)
-            push!(pre_computed_set_left, atom)
-        end
-    end
+function split_pre_computed_set!(
+	x,
+	pre_computed_set::Vector,
+	tree,
+	var::Int,
+	local_bounds::IntegerBounds;
+	atol = 1e-5,
+	rtol = 1e-5)
+	pre_computed_set_left = []
+	del_indices = []
+	for (idx, atom) in pairs(pre_computed_set)
+		if !is_bound_feasible(local_bounds, atom)
+			push!(del_indices, idx)
+			continue
+		end
 
-    return pre_computed_set_left, pre_computed_set_right
+		if atom[var] >= ceil(x[var]) || isapprox(atom[var], ceil(x[var]), atol = atol, rtol = rtol)
+			continue
+		elseif atom[var] <= floor(x[var]) || isapprox(atom[var], floor(x[var]), atol = atol, rtol = rtol)
+			push!(del_indices, idx)
+			push!(pre_computed_set_left, atom)
+		end
+
+	end
+	deleteat!(pre_computed_set, del_indices)
+
+	return pre_computed_set_left, pre_computed_set
 end
+
+build_dicg_start_point = nothing
+
+# function build_dicg_start_point(lmo::TimeTrackingLMO)
+# 	blmo = lmo.blmo
+# 	n, _ = get_list_of_variables(blmo)
+# 	d = ones(n)
+# 	x0 = FrankWolfe.compute_extreme_point(lmo, d)
+# 	return x0
+# end
+
+function dicg_start_point_initialize(lmo::TimeTrackingLMO, active_set::FrankWolfe.ActiveSet{T, R}, pre_computed_set) where {T, R}
+	if lmo.ncalls == 0
+		return FrankWolfe.get_active_set_iterate(active_set)
+	end
+	if pre_computed_set === nothing
+		x0 = build_dicg_start_point(lmo)
+	else
+		if !isempty(pre_computed_set)
+			# We pick a point by averaging the pre_computed_atoms as warm-start.  
+			num_pre_computed_set = length(pre_computed_set)
+			x0 = sum(pre_computed_set) / num_pre_computed_set
+		else
+			# We pick a random point.
+			d = FrankWolfe.get_active_set_iterate(active_set)
+			d = ones(length(d))
+			x0 = FrankWolfe.compute_extreme_point(lmo, d)
+		end
+	end
+end
+
+
 
 """
 Split a discarded vertices set between left and right children.
 """
 function split_vertices_set!(
-    discarded_set::FrankWolfe.DeletedVertexStorage{T},
-    tree,
-    var::Int,
-    x,
-    local_bounds::IntegerBounds;
-    atol=1e-5,
-    rtol=1e-5,
+	discarded_set::FrankWolfe.DeletedVertexStorage{T},
+	tree,
+	var::Int,
+	x,
+	local_bounds::IntegerBounds;
+	atol = 1e-5,
+	rtol = 1e-5,
 ) where {T}
-    right_as = FrankWolfe.DeletedVertexStorage{}(Vector{Float64}[], discarded_set.return_kth)
-    # indices to remove later from the left active set
-    left_del_indices = BitSet()
-    for (idx, vertex) in enumerate(discarded_set.storage)
-        if !is_bound_feasible(local_bounds, vertex)
-            push!(left_del_indices, idx)
-            continue
-        end
-        if vertex[var] >= ceil(x[var]) || isapprox(vertex[var], ceil(x[var]), atol=atol, rtol=rtol)
-            push!(right_as.storage, vertex)
-            push!(left_del_indices, idx)
-        elseif vertex[var] <= floor(x[var]) ||
-               isapprox(vertex[var], floor(x[var]), atol=atol, rtol=rtol)
-            # keep in left, don't add to right
-        else #floor(x[var]) < vertex[var] < ceil(x[var])
-            # if you are in middle, delete from the left and do not add to the right!
-            @warn "Attention! Vertex in the middle."
-            push!(left_del_indices, idx)
-        end
-    end
-    deleteat!(discarded_set.storage, left_del_indices)
-    return (discarded_set, right_as)
+	right_as = FrankWolfe.DeletedVertexStorage{}(Vector{Float64}[], discarded_set.return_kth)
+	# indices to remove later from the left active set
+	left_del_indices = BitSet()
+	for (idx, vertex) in enumerate(discarded_set.storage)
+		if !is_bound_feasible(local_bounds, vertex)
+			push!(left_del_indices, idx)
+			continue
+		end
+		if vertex[var] >= ceil(x[var]) || isapprox(vertex[var], ceil(x[var]), atol = atol, rtol = rtol)
+			push!(right_as.storage, vertex)
+			push!(left_del_indices, idx)
+		elseif vertex[var] <= floor(x[var]) ||
+			   isapprox(vertex[var], floor(x[var]), atol = atol, rtol = rtol)
+			# keep in left, don't add to right
+		else #floor(x[var]) < vertex[var] < ceil(x[var])
+			# if you are in middle, delete from the left and do not add to the right!
+			@warn "Attention! Vertex in the middle."
+			push!(left_del_indices, idx)
+		end
+	end
+	deleteat!(discarded_set.storage, left_del_indices)
+	return (discarded_set, right_as)
 end
 
-function is_bound_feasible(bounds::IntegerBounds, v; atol=1e-5)
-    for (idx, set) in bounds.lower_bounds
-        if v[idx] < set - atol
-            return false
-        end
-    end
-    for (idx, set) in bounds.upper_bounds
-        if v[idx] > set + atol
-            return false
-        end
-    end
-    return true
+function is_bound_feasible(bounds::IntegerBounds, v; atol = 1e-5)
+	for (idx, set) in bounds.lower_bounds
+		if v[idx] < set - atol
+			return false
+		end
+	end
+	for (idx, set) in bounds.upper_bounds
+		if v[idx] > set + atol
+			return false
+		end
+	end
+	return true
 end
 
 """
@@ -188,21 +235,21 @@ Checks if the branch and bound can be stopped.
 By default (in Bonobo) stops then the priority queue is empty. 
 """
 function Bonobo.terminated(tree::Bonobo.BnBTree{<:FrankWolfeNode})
-    if tree.root.problem.solving_stage == TIME_LIMIT_REACHED
-        return true
-    end
-    absgap = tree.incumbent - tree.lb
-    if absgap ≤ tree.options.abs_gap_limit
-        return true
-    end
-    dual_gap = if signbit(tree.incumbent) != signbit(tree.lb)
-        Inf
-    elseif tree.incumbent == tree.lb
-        0.0
-    else
-        absgap / min(abs(tree.incumbent), abs(tree.lb))
-    end
-    return isempty(tree.nodes) || dual_gap ≤ tree.options.dual_gap_limit
+	if tree.root.problem.solving_stage == TIME_LIMIT_REACHED
+		return true
+	end
+	absgap = tree.incumbent - tree.lb
+	if absgap ≤ tree.options.abs_gap_limit
+		return true
+	end
+	dual_gap = if signbit(tree.incumbent) != signbit(tree.lb)
+		Inf
+	elseif tree.incumbent == tree.lb
+		0.0
+	else
+		absgap / min(abs(tree.incumbent), abs(tree.lb))
+	end
+	return isempty(tree.nodes) || dual_gap ≤ tree.options.dual_gap_limit
 end
 
 
@@ -211,36 +258,36 @@ Naive optimization by enumeration.
 Default uses binary values.
 Otherwise, third argument should be a vector of n sets of possible values for the variables.
 """
-function min_via_enum(f, n, values=fill(0:1, n))
-    solutions = Iterators.product(values...)
-    best_val = Inf
-    best_sol = nothing
-    for sol in solutions
-        sol_vec = collect(sol)
-        val = f(sol_vec)
-        if best_val > val
-            best_val = val
-            best_sol = sol_vec
-        end
-    end
-    return best_val, best_sol
+function min_via_enum(f, n, values = fill(0:1, n))
+	solutions = Iterators.product(values...)
+	best_val = Inf
+	best_sol = nothing
+	for sol in solutions
+		sol_vec = collect(sol)
+		val = f(sol_vec)
+		if best_val > val
+			best_val = val
+			best_sol = sol_vec
+		end
+	end
+	return best_val, best_sol
 end
 
-function sparse_min_via_enum(f, n, k, values=fill(0:1, n))
-    solutions = Iterators.product(values...)
-    best_val = Inf
-    best_sol = nothing
-    for sol in solutions
-        sol_vec = collect(sol)
-        if sum(Int.(iszero.(sol_vec))) >= (n - k)
-            val = f(sol_vec)
-            if best_val > val
-                best_val = val
-                best_sol = sol_vec
-            end
-        end
-    end
-    return best_val, best_sol
+function sparse_min_via_enum(f, n, k, values = fill(0:1, n))
+	solutions = Iterators.product(values...)
+	best_val = Inf
+	best_sol = nothing
+	for sol in solutions
+		sol_vec = collect(sol)
+		if sum(Int.(iszero.(sol_vec))) >= (n - k)
+			val = f(sol_vec)
+			if best_val > val
+				best_val = val
+				best_sol = sol_vec
+			end
+		end
+	end
+	return best_val, best_sol
 end
 
 
