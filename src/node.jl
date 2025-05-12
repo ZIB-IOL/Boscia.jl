f80f53a50cb30951ccf4d7a4fd1484c56b010399
Hendrych <dhendryc@mob-009366.zib.de>
2025-05-12 09:05:19 +0200

    Propagate bounds before solving the node.

--- src/node.jl
+++ src/node.jl
@@ -268,6 +268,9 @@ function Bonobo.evaluate_node!(tree::Bonobo.BnBTree, node::FrankWolfeNode)
         for (_, v) in node.active_set
             @assert is_linear_feasible(tree.root.problem.tlmo, v)
         end
+
+    if tree.root.options[:propagate_bounds] !== nothing
+        tree.root.options[:propagate_bounds](tree, node)
     end
 
     # time tracking FW
