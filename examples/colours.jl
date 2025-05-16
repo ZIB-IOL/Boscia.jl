f00cd46e3604bcdc11893a2dbbdfbdd98a9145f5
Hendrych <dhendryc@mob-009366.zib.de>
2025-05-16 07:03:20 +0200

    Colour blind colours.

---
+++ examples/colours.jl
@@ -0,0 +1,38 @@
+# Define custom RGB colors (values between 0 and 1)
+
+# Color definitions as RGB tuples (values between 0 and 1)
+const labelkey = (0.0, 0.08, 0.45)
+const refkey = (0.0, 0.6, 0.0)
+const Brown = (0.45, 0.0, 0.05)
+const dgreen = (0.00, 0.49, 0.00)
+const dblue = (0.0, 0.08, 0.75)
+const ffwwqq = (1.0, 0.4, 0.0)
+const qqzzqq = (0.0, 0.6, 0.0)
+const qqqqff = (0.0, 0.0, 1.0)
+const dred = (217/255, 4/255, 4/255)  # D90404
+const orng = (211/255, 84/255, 0.0)   # D35400
+
+# ColorBrewer colors
+const cb_black = (0.0, 0.0, 0.0)
+const cb_blue_green = (0.0, 73/255, 73/255)
+const cb_green_sea = (0.0, 146/255, 146/255)
+const cb_rose = (255/255, 109/255, 182/255)
+const cb_salmon_pink = (255/255, 182/255, 119/255)
+const cb_purple = (73/255, 0.0, 146/255)
+const cb_blue = (0.0, 109/255, 219/255)
+const cb_lilac = (182/255, 109/255, 255/255)
+const cb_blue_sky = (109/255, 182/255, 255/255)
+const cb_blue_light = (182/255, 219/255, 255/255)
+const cb_burgundy = (146/255, 0.0, 0.0)
+const cb_brown = (146/255, 73/255, 0.0)
+const cb_clay = (219/255, 209/255, 0.0)
+const cb_green_lime = (36/255, 255/255, 36/255)
+const cb_yellow = (255/255, 255/255, 109/255)
+
+# Basic colors
+const bred = (1.0, 0.0, 0.0)          # FF0000
+const bpurp = (191/255, 0.0, 191/255) # BF00BF
+const bblu = (0.0, 0.0, 1.0)          # 0000FF
+const bcyan = (0.0, 191/255, 191/255) # 00BFBF
+const byellow = (191/255, 191/255, 0.0) # BFBF00
+const bgreen = (0.0, 128/255, 0.0)    # 008000
