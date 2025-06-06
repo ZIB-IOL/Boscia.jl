c7f2da4a22fb9d531976f313145b6d8d8988da36
Deborah Hendrych <d.hendrych@web.de>
2025-06-06 16:33:03 +0200

    Minor fix.

--- src/tightenings.jl
+++ src/tightenings.jl
@@ -18,77 +18,76 @@ function dual_tightening(tree, node, x, dual_gap)
         if rhs < 0
             @debug "Skipping tightening because rhs is negative: $rhs"
             return
-        else
-            for j in tree.root.problem.integer_variables
-                lb_global = get(tree.root.problem.integer_variable_bounds, (j, :greaterthan), -Inf)
-                ub_global = get(tree.root.problem.integer_variable_bounds, (j, :lessthan), Inf)
-                lb = get(node.local_bounds.lower_bounds, j, lb_global)
-                ub = get(node.local_bounds.upper_bounds, j, ub_global)
-                @assert lb >= lb_global
-                @assert ub <= ub_global
-                if lb ≈ ub
-                    # variable already fixed
-                    continue
+        end
+        for j in tree.root.problem.integer_variables
+            lb_global = get(tree.root.problem.integer_variable_bounds, (j, :greaterthan), -Inf)
+            ub_global = get(tree.root.problem.integer_variable_bounds, (j, :lessthan), Inf)
+            lb = get(node.local_bounds.lower_bounds, j, lb_global)
+            ub = get(node.local_bounds.upper_bounds, j, ub_global)
+            @assert lb >= lb_global
+            @assert ub <= ub_global
+            if lb ≈ ub
+                # variable already fixed
+                continue
+            end
+            gj = grad[j]
+            if ≈(x[j], lb, atol=tree.options.atol, rtol=tree.options.rtol)
+                if !isapprox(gj, 0, atol=1e-5)
+                    num_potential_tightenings += 1
                 end
-                gj = grad[j]
-                if ≈(x[j], lb, atol=tree.options.atol, rtol=tree.options.rtol)
-                    if !isapprox(gj, 0, atol=1e-5)
-                        num_potential_tightenings += 1
-                    end
-                    if gj > 0
-                        Mlb = 0
-                        bound_tightened = true
-                        @debug "starting tightening ub $(rhs)"
-                        while 0.99 * (Mlb * gj + μ / 2 * Mlb^2) <= rhs
-                            Mlb += 1
-                            if lb + Mlb - 1 == ub
-                                bound_tightened = false
-                                break
-                            end
-                        end
-                        if bound_tightened
-                            new_bound = lb + Mlb - 1
-                            @debug "found UB tightening $ub -> $new_bound"
-                            node.local_bounds[j, :lessthan] = new_bound
-                            num_tightenings += 1
-                            if haskey(tree.root.problem.integer_variable_bounds, (j, :lessthan))
-                                @assert node.local_bounds[j, :lessthan] <=
-                                        tree.root.problem.integer_variable_bounds[j, :lessthan]
-                            end
+                if gj > 0
+                    Mlb = 0
+                    bound_tightened = true
+                    @debug "starting tightening ub $(rhs)"
+                    while 0.99 * (Mlb * gj + μ / 2 * Mlb^2) <= rhs
+                        Mlb += 1
+                        if lb + Mlb - 1 == ub
+                            bound_tightened = false
+                            break
                         end
                     end
-                elseif ≈(x[j], ub, atol=tree.options.atol, rtol=tree.options.rtol)
-                    if !isapprox(gj, 0, atol=1e-5)
-                        num_potential_tightenings += 1
-                    end
-                    if gj < 0
-                        Mub = 0
-                        bound_tightened = true
-                        @debug "starting tightening lb $(rhs)"
-                        while -0.99 * (Mub * gj + μ / 2 * Mub^2) <= rhs
-                            Mub += 1
-                            if ub - Mub + 1 == lb
-                                bound_tightened = false
-                                break
-                            end
+                    if bound_tightened
+                        new_bound = lb + Mlb - 1
+                        @debug "found UB tightening $ub -> $new_bound"
+                        node.local_bounds[j, :lessthan] = new_bound
+                        num_tightenings += 1
+                        if haskey(tree.root.problem.integer_variable_bounds, (j, :lessthan))
+                            @assert node.local_bounds[j, :lessthan] <=
+                                    tree.root.problem.integer_variable_bounds[j, :lessthan]
                         end
-                        if bound_tightened
-                            new_bound = ub - Mub + 1
-                            @debug "found LB tightening $lb -> $new_bound"
-                            node.local_bounds[j, :greaterthan] = new_bound
-                            num_tightenings += 1
-                            if haskey(tree.root.problem.integer_variable_bounds, (j, :greaterthan))
-                                @assert node.local_bounds[j, :greaterthan] >=
-                                        tree.root.problem.integer_variable_bounds[j, :greaterthan]
-                            end
+                    end
+                end
+            elseif ≈(x[j], ub, atol=tree.options.atol, rtol=tree.options.rtol)
+                if !isapprox(gj, 0, atol=1e-5)
+                    num_potential_tightenings += 1
+                end
+                if gj < 0
+                    Mub = 0
+                    bound_tightened = true
+                    @debug "starting tightening lb $(rhs)"
+                    while -0.99 * (Mub * gj + μ / 2 * Mub^2) <= rhs
+                        Mub += 1
+                        if ub - Mub + 1 == lb
+                            bound_tightened = false
+                            break
+                        end
+                    end
+                    if bound_tightened
+                        new_bound = ub - Mub + 1
+                        @debug "found LB tightening $lb -> $new_bound"
+                        node.local_bounds[j, :greaterthan] = new_bound
+                        num_tightenings += 1
+                        if haskey(tree.root.problem.integer_variable_bounds, (j, :greaterthan))
+                            @assert node.local_bounds[j, :greaterthan] >=
+                                    tree.root.problem.integer_variable_bounds[j, :greaterthan]
                         end
                     end
                 end
             end
-            @debug "# tightenings $num_tightenings"
-            node.local_tightenings = num_tightenings
-            node.local_potential_tightenings = num_potential_tightenings
         end
+        @debug "# tightenings $num_tightenings"
+        node.local_tightenings = num_tightenings
+        node.local_potential_tightenings = num_potential_tightenings
     end
 end
 
