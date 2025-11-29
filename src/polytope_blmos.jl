407c4bb27bd145465bb12771c0584b4a639775e8
LJS42 <1457928939@qq.com>
2025-11-29 19:56:07 +0100

    format check

--- src/polytope_blmos.jl
+++ src/polytope_blmos.jl
@@ -957,15 +957,15 @@ function rounding_hyperplane_heuristic(
     # Round integer variables
     for idx in int_idx
         τ_int = τ >= 0 ? floor(Int, τ) : ceil(Int, τ)
-        
+
         if abs(x[idx]) >= abs(τ) - 0.5
             z_temp = sign(x[idx]) * τ_int
         else
             z_temp = round(x[idx])
         end
-        
+
         z[idx] = max(lb[idx], min(ub[idx], z_temp))
-        z[idx] = round(Int, z[idx]) 
+        z[idx] = round(Int, z[idx])
     end
 
     #Compute budget usage (L₁)
@@ -984,8 +984,8 @@ function rounding_hyperplane_heuristic(
 
     #decrease integer vars by 1 (toward zero)
     while LinearAlgebra.norm(z, 1) > L1_limit
-        z = reduce_from_max(z, lb, ub, int_idx) 
-        
+        z = reduce_from_max(z, lb, ub, int_idx)
+
         if all(z[int_idx] .== lb[int_idx]) && !isempty(cont_idx)
             z = reduce_continuous!(z, cont_idx)
         elseif all(z[int_idx] .== lb[int_idx]) && isempty(cont_idx)
@@ -1003,20 +1003,20 @@ function reduce_from_max(z, lower_bounds, upper_bounds, int_idx)
     if isempty(int_idx)
         return z
     end
-    
+
     absvals = abs.(z[int_idx])
     i = int_idx[argmax(absvals)]
-    
+
     if z[i] > 0
         z[i] = max(lower_bounds[i], z[i] - 1)
-        
+
     elseif z[i] < 0
         z[i] = min(upper_bounds[i], z[i] + 1)
-        
+
     end
-    
-    z[i] = round(z[i]) 
-    
+
+    z[i] = round(z[i])
+
     return z
 end
 
