2b6446bddbcf2d1e178e124d88c81866e874a3f1
LJS42 <1457928939@qq.com>
2025-12-10 11:24:27 +0100

    update the format

--- test/heuristics.jl
+++ test/heuristics.jl
@@ -20,7 +20,7 @@ seed = rand(UInt64)
 rng = StableRNG(seed)
 
 n = 20
-x_sol = rand(rng, 1:floor(Int, n/4), n)
+x_sol = rand(rng, 1:floor(Int, n / 4), n)
 N = sum(x_sol)
 dir = vcat(fill(1, floor(Int, n / 2)), fill(-1, floor(Int, n / 2)), fill(0, mod(n, 2)))
 diffi = x_sol + 0.3 * dir
@@ -53,7 +53,7 @@ diffi = x_sol + 0.3 * dir
 end
 
 n = 20
-x_sol = rand(rng, 1:floor(Int, n/4), n)
+x_sol = rand(rng, 1:floor(Int, n / 4), n)
 diffi = x_sol + 0.3 * rand(rng, [-1, 1], n)
 
 @testset "Hyperplane Aware Rounding - Unit Simplex" begin
@@ -222,5 +222,5 @@ end
 
 n = 20
 sparsity = 0.3
-x_sol = [rand() < sparsity ? 0 : rand(1:floor(Int, n/4)) for _ in 1:n]
+x_sol = [rand() < sparsity ? 0 : rand(1:floor(Int, n / 4)) for _ in 1:n]
 n_int = 17
