d60367ce3d37be104141b037b6fb4a34da0abae2
WenjieXiao-2022 <shawwj2020@126.com>
2025-12-19 14:52:29 +0100

    add traverse strategies tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,6 +28,7 @@ using Aqua
     include("time_limit.jl")
     include("strong_convexity_and_sharpness.jl")
     include("branching_strategy_test.jl")
+    include("traverse_strategy_test.jl")
 
     # Files to exclude from testing (e.g., utilities that require extra dependencies)
     excluded_files = ["plot_utilities.jl"]
