3eaf2a59a43f2cca429f82d0fdbf2b3566934773
LJS42 <1457928939@qq.com>
2026-02-04 17:09:52 +0100

    fix

--- test/custom_lmo_testing.jl
+++ test/custom_lmo_testing.jl
@@ -52,3 +52,52 @@ using FrankWolfe
     # compare objective values (not pointwise equality!)
     @test isapprox(v_lmo, v_moi; atol=1e-6)
 end
+
+@testset "LMO on inface vs MOI on the same feasible region - K" begin
+    n = 10
+    K = 5
+    rhs = 3.0
+
+    direction = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    lb = fill(-7.0, n)
+    ub = fill(7.0, n)
+    int_vars = [2, 5, 7]
+    x = zeros(n)
+    x[1] = 3.0
+
+    # --- LMO ---
+    lmo = FrankWolfe.KSparseLMO(K, rhs)
+
+    v_lmo = Boscia.bounded_compute_inface_extreme_point(lmo, direction, x, lb, ub, int_vars)
+
+    # --- MOI ---
+    model = Model(HiGHS.Optimizer)
+
+    # variables v with bounds
+    @variable(model, lb[i] <= v[i=1:n] <= ub[i])
+
+    # integer variables
+    for i in int_vars
+        set_integer(v[i])
+    end
+
+    @variable(model, abs_v[i=1:n] >= 0)
+
+    for i in 1:n
+        @constraint(model, abs_v[i] <= rhs)
+        @constraint(model, abs_v[i] >= v[i])
+        @constraint(model, abs_v[i] >= -v[i])
+    end
+
+    @constraint(model, sum(abs_v[i] for i in 1:n) <= K * rhs)
+
+    # objective
+    @objective(model, Min, sum(direction[i] * v[i] for i in 1:n))
+
+    optimize!(model)
+
+    v_moi = value.(v)
+
+    # compare objective values (not pointwise equality!)
+    @test isapprox(v_lmo, v_moi; atol=1e-6)
+end
