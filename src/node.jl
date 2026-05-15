ce4361310428563dbd4bf1369daa4057eafe0267
Deborah Hendrych <d.hendrych@web.de>
2026-05-15 12:58:45 +0200

    Node callback is only interesting in the smoothing case.

--- src/node.jl
+++ src/node.jl
@@ -474,11 +474,11 @@ function Bonobo.evaluate_node!(tree::Bonobo.BnBTree, node::FrankWolfeNode)
             return NaN, NaN
         end
     end
-    if tree.root.options[:node_callback] !== nothing
-        tree.root.options[:node_callback](tree, node, x, primal, dual_gap, fw_status, atoms_set)
-    end
 
     if tree.root.options[:mode] == SMOOTHING_MODE && tree.root.options[:use_sub_grad_info]
+        if tree.root.options[:node_callback] !== nothing
+            tree.root.options[:node_callback](tree, node, x, primal, dual_gap, fw_status, atoms_set)
+        end
         original_primal = tree.root.options[:original_objective](x)
          @assert primal <= original_primal + 1e-10 "primal = $(primal) > original_primal + 1e-10 = $(original_primal + 1e-10)"
         if original_primal > tree.root.options[:local_opt_primal] && tree.root.options[:best_sol_by_original]
